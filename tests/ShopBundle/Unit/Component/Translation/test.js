7bc80ab3a54b9ed30f82127e19ad16eccdba72b5
Tomas Ludvik <tomas.ludvik@shopsys.com>
2018-03-22 06:51:38 +0100

    all unit tests that are part of backend are now located in framework package

--- tests/ShopBundle/Unit/Component/Translation/test.js
+++
@@ -1,9 +0,0 @@
-Shopsys.translator.trans('trans test');
-
-Shopsys.translator.transChoice('transChoice test', 5);
-
-Shopsys.translator.trans('trans test with domain', {}, 'testDomain');
-
-Shopsys.translator.transChoice('transChoice test with domain', 5, [], 'testDomain');
-
-Shopsys.translator.trans('concatenated' + ' ' + 'message');
