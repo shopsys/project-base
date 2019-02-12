effb55943b8fb8a15e766e57abeaf81d5325e30d
Mathew Czerner <matous.czerner@shopsys.com>
2019-02-12 09:56:00 +0100

    WIP

--- .ci/deploy-to-google-cloud.sh
+++ .ci/deploy-to-google-cloud.sh
@@ -51,8 +51,8 @@ yq write --inplace kubernetes/deployments/webserver-php-fpm.yml spec.template.sp
 yq write --inplace kubernetes/deployments/webserver-php-fpm.yml spec.template.spec.initContainers[1].env[1].value ${PROJECT_ID}
 
 # Set domain urls
-yq write --inplace app/config/domains_urls.yml domains_urls[0].url http://${FIRST_DOMAIN_HOSTNAME}
-yq write --inplace app/config/domains_urls.yml domains_urls[1].url http://${SECOND_DOMAIN_HOSTNAME}
+yq write --inplace app/config/domains_urls.yml domains_urls[0].url https://${FIRST_DOMAIN_HOSTNAME}
+yq write --inplace app/config/domains_urls.yml domains_urls[1].url https://${SECOND_DOMAIN_HOSTNAME}
 
 # Add a mask for trusted proxies so that load balanced traffic is trusted and headers from outside of the network are not lost
 yq write --inplace app/config/parameters.yml parameters.trusted_proxies[+] 10.0.0.0/8
