shopsys/shopsys

commit d7ead3d372b96bea41e61647c4b14aec6b867901

ProgressBar is now created via ProgressBarFactory and does no longer support speed and step duration
- ProgressBar is final class in Symfony 3.4 and cannot be extended
- speed and step duration is no longer supported because it is no longer possible to add microtimes in ProgressBar class

src/Shopsys/ShopBundle/DataFixtures/Performance/CategoryDataFixture.php
@@ -3,12 +3,13 @@
 namespace Shopsys\ShopBundle\DataFixtures\Performance;
 
 use Faker\Generator as Faker;
-use Shopsys\ShopBundle\Component\Console\ProgressBar;
+use Shopsys\ShopBundle\Component\Console\ProgressBarFactory;
 use Shopsys\ShopBundle\Component\DataFixture\PersistentReferenceFacade;
 use Shopsys\ShopBundle\Component\Doctrine\SqlLoggerFacade;
 use Shopsys\ShopBundle\Model\Category\Category;
 use Shopsys\ShopBundle\Model\Category\CategoryData;
 use Shopsys\ShopBundle\Model\Category\CategoryFacade;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class CategoryDataFixture
@@ -46,18 +47,25 @@ class CategoryDataFixture
     private $persistentReferenceFacade;
 
     /**
+     * @var \Shopsys\ShopBundle\Component\Console\ProgressBarFactory
+     */
+    private $progressBarFactory;
+
+    /**
      * @param int[] $categoryCountsByLevel
      * @param \Shopsys\ShopBundle\Model\Category\CategoryFacade $categoryFacade
      * @param \Shopsys\ShopBundle\Component\Doctrine\SqlLoggerFacade $sqlLoggerFacade
      * @param \Shopsys\ShopBundle\Component\DataFixture\PersistentReferenceFacade $persistentReferenceFacade
      * @param \Faker\Generator $faker
+     * @param \Shopsys\ShopBundle\Component\Console\ProgressBarFactory $progressBarFactory
      */
     public function __construct(
         $categoryCountsByLevel,
         CategoryFacade $categoryFacade,
         SqlLoggerFacade $sqlLoggerFacade,
         PersistentReferenceFacade $persistentReferenceFacade,
-        Faker $faker
+        Faker $faker,
+        ProgressBarFactory $progressBarFactory
     ) {
         $this->categoryCountsByLevel = $categoryCountsByLevel;
         $this->categoryFacade = $categoryFacade;
@@ -65,6 +73,7 @@ class CategoryDataFixture
         $this->faker = $faker;
         $this->categoriesCreated = 0;
         $this->persistentReferenceFacade = $persistentReferenceFacade;
+        $this->progressBarFactory = $progressBarFactory;
     }
 
     /**
@@ -72,7 +81,7 @@ class CategoryDataFixture
      */
     public function load(OutputInterface $output)
     {
-        $progressBar = $this->createProgressBar($output);
+        $progressBar = $this->progressBarFactory->create($output, array_sum($this->categoryCountsByLevel));
 
         $rootCategory = $this->categoryFacade->getRootCategory();
         $this->sqlLoggerFacade->temporarilyDisableLogging();
@@ -134,22 +143,5 @@ class CategoryDataFixture
         $categoryData->parent = $parentCategory;
 
         return $categoryData;
-    }
-
-    /**
-     * @param \Symfony\Component\Console\Output\OutputInterface $output
-     * @return \Shopsys\ShopBundle\Component\Console\ProgressBar
-     */
-    private function createProgressBar(OutputInterface $output)
-    {
-        $progressBar = new ProgressBar($output, $this->recursivelyCountCategoriesInCategoryTree());
-        $progressBar->setFormat(
-            '%current%/%max% [%bar%] %percent:3s%%,%speed:6.1f% cat./s (%step_duration:.3f% s/cat.),'
-            . ' Elapsed: %elapsed_hms%, Remaining: %remaining_hms%, MEM:%memory:9s%'
-        );
-        $progressBar->setRedrawFrequency(10);
-        $progressBar->start();
-
-        return $progressBar;
     }
 }

src/Shopsys/ShopBundle/Component/Console/ProgressBar.php
@@ -1,104 +1,0 @@
-<?php
-
-namespace Shopsys\ShopBundle\Component\Console;
-
-use Symfony\Component\Console\Helper\ProgressBar as BaseProgressBar;
-use Symfony\Component\Console\Output\OutputInterface;
-
-/**
- * ProgressBar with advanced placeholders for displaying speed etc.
- */
-class ProgressBar extends BaseProgressBar
-{
-    /**
-     * @var float
-     */
-    private $microtimeAtLastDisplay;
-
-    /**
-     * @var int
-     */
-    private $progressAtLastDisplay;
-
-    /**
-     * @param \Symfony\Component\Console\Output\OutputInterface $output
-     * @param int $max
-     */
-    public function __construct(OutputInterface $output, $max = 0)
-    {
-        parent::__construct($output, $max);
-
-        $this->microtimeAtLastDisplay = microtime(true);
-        $this->progressAtLastDisplay = 0;
-
-        $this->initializeCustomPlaceholderFormatters();
-    }
-
-    private function initializeCustomPlaceholderFormatters()
-    {
-        self::setPlaceholderFormatterDefinition('speed', function () {
-            $microtimeSinceLastDisplay = microtime(true) - $this->microtimeAtLastDisplay;
-            $progressSinceLastDisplay = $this->getProgress() - $this->progressAtLastDisplay;
-
-            if ($microtimeSinceLastDisplay === 0) {
-                return 0;
-            }
-
-            return $progressSinceLastDisplay / $microtimeSinceLastDisplay;
-        });
-
-        self::setPlaceholderFormatterDefinition('step_duration', function () {
-            $microtimeSinceLastDisplay = microtime(true) - $this->microtimeAtLastDisplay;
-            $progressSinceLastDisplay = $this->getProgress() - $this->progressAtLastDisplay;
-
-            if ($progressSinceLastDisplay === 0) {
-                return 0;
-            }
-
-            return $microtimeSinceLastDisplay / $progressSinceLastDisplay;
-        });
-
-        self::setPlaceholderFormatterDefinition('remaining_hms', function () {
-            if (!$this->getMaxSteps()) {
-                throw new \LogicException('Unable to display the remaining time if the maximum number of steps is not set.');
-            }
-
-            if ($this->getProgress() !== 0) {
-                $secondsPerStep = (time() - $this->getStartTime()) / $this->getProgress();
-                $remainingSteps = $this->getMaxSteps() - $this->getProgress();
-
-                $remainingSeconds = round($secondsPerStep * $remainingSteps);
-            } else {
-                $remainingSeconds = 0;
-            }
-
-            return $this->formatTimeHms($remainingSeconds);
-        });
-
-        self::setPlaceholderFormatterDefinition('elapsed_hms', function () {
-            return $this->formatTimeHms(time() - $this->getStartTime());
-        });
-    }
-
-    public function display()
-    {
-        parent::display();
-
-        $this->microtimeAtLastDisplay = microtime(true);
-        $this->progressAtLastDisplay = $this->getProgress();
-    }
-
-    /**
-     * @param int $timeInSeconds
-     * @return string
-     */
-    private function formatTimeHms($timeInSeconds)
-    {
-        return sprintf(
-            '%dh %02dm %02ds',
-            floor($timeInSeconds / 3600),
-            floor(($timeInSeconds / 60) % 60),
-            floor($timeInSeconds % 60)
-        );
-    }
-}

src/Shopsys/ShopBundle/Component/Console/ProgressBarFactory.php
@@ -1,0 +1,61 @@
+<?php
+
+namespace Shopsys\ShopBundle\Component\Console;
+
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ProgressBarFactory
+{
+    /**
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     * @param int $max
+     * @return \Symfony\Component\Console\Helper\ProgressBar
+     */
+    public function create(OutputInterface $output, $max)
+    {
+        $bar = new ProgressBar($output, $max);
+        $this->initializeCustomPlaceholderFormatters();
+        $bar->setFormat(
+            '%current%/%max% [%bar%] %percent:3s%, Elapsed: %elapsed_hms%, Remaining: %remaining_hms%, MEM:%memory:9s%'
+        );
+        $bar->setRedrawFrequency(10);
+        $bar->start();
+
+        return $bar;
+    }
+
+    private function initializeCustomPlaceholderFormatters()
+    {
+        ProgressBar::setPlaceholderFormatterDefinition('remaining_hms', function (ProgressBar $bar) {
+            if ($bar->getProgress() !== 0) {
+                $secondsPerStep = (time() - $bar->getStartTime()) / $bar->getProgress();
+                $remainingSteps = $bar->getMaxSteps() - $bar->getProgress();
+
+                $remainingSeconds = round($secondsPerStep * $remainingSteps);
+            } else {
+                $remainingSeconds = 0;
+            }
+
+            return $this->formatTimeHms($remainingSeconds);
+        });
+
+        ProgressBar::setPlaceholderFormatterDefinition('elapsed_hms', function (ProgressBar $bar) {
+            return $this->formatTimeHms(time() - $bar->getStartTime());
+        });
+    }
+
+    /**
+     * @param int $timeInSeconds
+     * @return string
+     */
+    private function formatTimeHms($timeInSeconds)
+    {
+        return sprintf(
+            '%dh %02dm %02ds',
+            floor($timeInSeconds / 3600),
+            floor(($timeInSeconds / 60) % 60),
+            floor($timeInSeconds % 60)
+        );
+    }
+}

src/Shopsys/ShopBundle/DataFixtures/Performance/OrderDataFixture.php
@@ -4,7 +4,7 @@ namespace Shopsys\ShopBundle\DataFixtures\Performance;
 
 use Doctrine\ORM\EntityManager;
 use Faker\Generator as Faker;
-use Shopsys\ShopBundle\Component\Console\ProgressBar;
+use Shopsys\ShopBundle\Component\Console\ProgressBarFactory;
 use Shopsys\ShopBundle\Component\DataFixture\PersistentReferenceFacade;
 use Shopsys\ShopBundle\Component\Doctrine\EntityManagerFacade;
 use Shopsys\ShopBundle\Component\Doctrine\SqlLoggerFacade;
@@ -97,6 +97,11 @@ class OrderDataFixture
     private $customerFacade;
 
     /**
+     * @var \Shopsys\ShopBundle\Component\Console\ProgressBarFactory
+     */
+    private $progressBarFactory;
+
+    /**
      * @param int $orderTotalCount
      * @param int $orderItemCountPerOrder
      * @param \Doctrine\ORM\EntityManager $em
@@ -108,6 +113,7 @@ class OrderDataFixture
      * @param \Shopsys\ShopBundle\Model\Order\Preview\OrderPreviewFactory $orderPreviewFactory
      * @param \Shopsys\ShopBundle\Model\Product\ProductFacade $productFacade
      * @param \Shopsys\ShopBundle\Model\Customer\CustomerFacade $customerFacade
+     * @param \Shopsys\ShopBundle\Component\Console\ProgressBarFactory $progressBarFactory
      */
     public function __construct(
         $orderTotalCount,
@@ -120,7 +126,8 @@ class OrderDataFixture
         OrderFacade $orderFacade,
         OrderPreviewFactory $orderPreviewFactory,
         ProductFacade $productFacade,
-        CustomerFacade $customerFacade
+        CustomerFacade $customerFacade,
+        ProgressBarFactory $progressBarFactory
     ) {
         $this->orderTotalCount = $orderTotalCount;
         $this->orderItemCountPerOrder = $orderItemCountPerOrder;
@@ -134,6 +141,7 @@ class OrderDataFixture
         $this->orderPreviewFactory = $orderPreviewFactory;
         $this->productFacade = $productFacade;
         $this->customerFacade = $customerFacade;
+        $this->progressBarFactory = $progressBarFactory;
     }
 
     /**
@@ -147,13 +155,7 @@ class OrderDataFixture
         $this->loadPerformanceProductIds();
         $this->loadPerformanceUserIdsOnFirstDomain();
 
-        $progressBar = new ProgressBar($output, $this->orderTotalCount);
-        $progressBar->setFormat(
-            '%current%/%max% [%bar%] %percent:3s%%,%speed:6.1f% orders/s (%step_duration:.3f% s/ord.),'
-            . ' Elapsed: %elapsed_hms%, Remaining: %remaining_hms%, MEM:%memory:9s%'
-        );
-        $progressBar->setRedrawFrequency(10);
-        $progressBar->start();
+        $progressBar = $this->progressBarFactory->create($output, $this->orderTotalCount);
 
         for ($orderIndex = 0; $orderIndex < $this->orderTotalCount; $orderIndex++) {
             $this->createOrder();

src/Shopsys/ShopBundle/DataFixtures/Performance/ProductDataFixture.php
@@ -4,7 +4,7 @@ namespace Shopsys\ShopBundle\DataFixtures\Performance;
 
 use Doctrine\ORM\EntityManager;
 use Faker\Generator as Faker;
-use Shopsys\ShopBundle\Component\Console\ProgressBar;
+use Shopsys\ShopBundle\Component\Console\ProgressBarFactory;
 use Shopsys\ShopBundle\Component\DataFixture\PersistentReferenceFacade;
 use Shopsys\ShopBundle\Component\DataFixture\ProductDataFixtureReferenceInjector;
 use Shopsys\ShopBundle\Component\Doctrine\EntityManagerFacade;
@@ -113,6 +113,11 @@ class ProductDataFixture
     private $productDataFixtureCsvReader;
 
     /**
+     * @var \Shopsys\ShopBundle\Component\Console\ProgressBarFactory
+     */
+    private $progressBarFactory;
+
+    /**
      * @param int $productTotalCount
      * @param \Doctrine\ORM\EntityManager $em
      * @param \Shopsys\ShopBundle\Component\Doctrine\EntityManagerFacade $entityManagerFacade
@@ -127,6 +132,7 @@ class ProductDataFixture
      * @param \Shopsys\ShopBundle\Model\Product\Availability\ProductAvailabilityRecalculationScheduler $productAvailabilityRecalculationScheduler
      * @param \Shopsys\ShopBundle\Model\Product\Pricing\ProductPriceRecalculationScheduler $productPriceRecalculationScheduler
      * @param \Shopsys\ShopBundle\DataFixtures\Demo\ProductDataFixtureCsvReader $productDataFixtureCsvReader
+     * @param \Shopsys\ShopBundle\Component\Console\ProgressBarFactory $progressBarFactory
      */
     public function __construct(
         $productTotalCount,
@@ -142,7 +148,8 @@ class ProductDataFixture
         Faker $faker,
         ProductAvailabilityRecalculationScheduler $productAvailabilityRecalculationScheduler,
         ProductPriceRecalculationScheduler $productPriceRecalculationScheduler,
-        ProductDataFixtureCsvReader $productDataFixtureCsvReader
+        ProductDataFixtureCsvReader $productDataFixtureCsvReader,
+        ProgressBarFactory $progressBarFactory
     ) {
         $this->productTotalCount = $productTotalCount;
         $this->em = $em;
@@ -160,6 +167,7 @@ class ProductDataFixture
         $this->productAvailabilityRecalculationScheduler = $productAvailabilityRecalculationScheduler;
         $this->productPriceRecalculationScheduler = $productPriceRecalculationScheduler;
         $this->productDataFixtureCsvReader = $productDataFixtureCsvReader;
+        $this->progressBarFactory = $progressBarFactory;
     }
 
     /**
@@ -176,13 +184,7 @@ class ProductDataFixture
             $csvRows
         );
 
-        $progressBar = new ProgressBar($output, $this->productTotalCount);
-        $progressBar->setFormat(
-            '%current%/%max% [%bar%] %percent:3s%%,%speed:6.1f% prod./s (%step_duration:.3f% s/prod.),'
-            . ' Elapsed: %elapsed_hms%, Remaining: %remaining_hms%, MEM:%memory:9s%'
-        );
-        $progressBar->setRedrawFrequency(10);
-        $progressBar->start();
+        $progressBar = $this->progressBarFactory->create($output, $this->productTotalCount);
 
         while ($this->countImported < $this->productTotalCount) {
             $row = next($csvRows);

src/Shopsys/ShopBundle/DataFixtures/Performance/UserDataFixture.php
@@ -3,7 +3,7 @@
 namespace Shopsys\ShopBundle\DataFixtures\Performance;
 
 use Faker\Generator as Faker;
-use Shopsys\ShopBundle\Component\Console\ProgressBar;
+use Shopsys\ShopBundle\Component\Console\ProgressBarFactory;
 use Shopsys\ShopBundle\Component\DataFixture\PersistentReferenceFacade;
 use Shopsys\ShopBundle\Component\Doctrine\EntityManagerFacade;
 use Shopsys\ShopBundle\Component\Doctrine\SqlLoggerFacade;
@@ -61,6 +61,11 @@ class UserDataFixture
     private $persistentReferenceFacade;
 
     /**
+     * @var \Shopsys\ShopBundle\Component\Console\ProgressBarFactory
+     */
+    private $progressBarFactory;
+
+    /**
      * @param int $userCountPerDomain
      * @param \Shopsys\ShopBundle\Component\Doctrine\EntityManagerFacade $entityManagerFacade
      * @param \Shopsys\ShopBundle\Component\Domain\Domain $domain
@@ -69,6 +74,7 @@ class UserDataFixture
      * @param \Shopsys\ShopBundle\Model\Customer\UserDataFactory $userDataFactory
      * @param \Faker\Generator $faker
      * @param \Shopsys\ShopBundle\Component\DataFixture\PersistentReferenceFacade $persistentReferenceFacade
+     * @param \Shopsys\ShopBundle\Component\Console\ProgressBarFactory $progressBarFactory
      */
     public function __construct(
         $userCountPerDomain,
@@ -78,7 +84,8 @@ class UserDataFixture
         CustomerFacade $customerEditFacade,
         UserDataFactory $userDataFactory,
         Faker $faker,
-        PersistentReferenceFacade $persistentReferenceFacade
+        PersistentReferenceFacade $persistentReferenceFacade,
+        ProgressBarFactory $progressBarFactory
     ) {
         $this->entityManagerFacade = $entityManagerFacade;
         $this->domain = $domain;
@@ -88,6 +95,7 @@ class UserDataFixture
         $this->faker = $faker;
         $this->persistentReferenceFacade = $persistentReferenceFacade;
         $this->userCountPerDomain = $userCountPerDomain;
+        $this->progressBarFactory = $progressBarFactory;
     }
 
     /**
@@ -97,12 +105,13 @@ class UserDataFixture
     {
         // Sql logging during mass data import makes memory leak
         $this->sqlLoggerFacade->temporarilyDisableLogging();
+        $domains = $this->domain->getAll();
 
-        $progressBar = $this->createProgressBar($output);
+        $progressBar = $this->progressBarFactory->create($output, count($domains) * $this->userCountPerDomain);
 
         $isFirstUser = true;
 
-        foreach ($this->domain->getAll() as $domainConfig) {
+        foreach ($domains as $domainConfig) {
             for ($i = 0; $i < $this->userCountPerDomain; $i++) {
                 $user = $this->createCustomerOnDomain($domainConfig->getId(), $i);
                 $progressBar->advance();
@@ -179,22 +188,5 @@ class UserDataFixture
         $customerData->deliveryAddressData = $deliveryAddressData;
 
         return $customerData;
-    }
-
-    /**
-     * @param \Symfony\Component\Console\Output\OutputInterface $output
-     * @return \Shopsys\ShopBundle\Component\Console\ProgressBar
-     */
-    private function createProgressBar(OutputInterface $output)
-    {
-        $progressBar = new ProgressBar($output, $this->userCountPerDomain * count($this->domain->getAll()));
-        $progressBar->setFormat(
-            '%current%/%max% [%bar%] %percent:3s%%,%speed:6.1f% users/s (%step_duration:.3f% s/user),'
-            . ' Elapsed: %elapsed_hms%, Remaining: %remaining_hms%, MEM:%memory:9s%'
-        );
-        $progressBar->setRedrawFrequency(10);
-        $progressBar->start();
-
-        return $progressBar;
     }
 }

shopsys/project-base

commit 25022a0731bece0f59aa9b9ffa06294273913fa3

Performance\CategoryDataFixture: fix maximum count in progress bar (#902)
- the numbers in the array $categoryCountsByLevel define how many categories should be created on each level
- private method recursivelyCountCategoriesInCategoryTree should be used instead of array_sum
- error introduced in d7ead3d372b96bea41e61647c4b14aec6b867901 when the ProgressBarFactory was created

src/Shopsys/ShopBundle/DataFixtures/Performance/CategoryDataFixture.php
@@ -89,7 +89,7 @@ class CategoryDataFixture
      */
     public function load(OutputInterface $output)
     {
-        $progressBar = $this->progressBarFactory->create($output, array_sum($this->categoryCountsByLevel));
+        $progressBar = $this->progressBarFactory->create($output, $this->recursivelyCountCategoriesInCategoryTree());
 
         $rootCategory = $this->categoryFacade->getRootCategory();
         $this->sqlLoggerFacade->temporarilyDisableLogging();