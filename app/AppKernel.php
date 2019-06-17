pesovo/shopsys

added shopsys/api bundle
- working endpoints
    - /api/v1/products (includes filter by uuids, pagination)
    - /api/v1/products/{uuid}
- using FOSRestBundle
- for finding products from facade is used query so the code is prepared for further extension by extending the ProductQuery and the query method ProductRepository::findByQuery()
- routes in project-base must be added for each version - that is a compromise between "routes works automatically when I add the bundle" and "user have to add all routes manually"
- api extension works form project-base in a way "what is extended is taken from project-base; what is not extended is taken from framework"

project-base/app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new FM\ElfinderBundle\FMElfinderBundle(),
             new Fp\JsFormValidatorBundle\FpJsFormValidatorBundle(),
             new Intaro\PostgresSearchBundle\IntaroPostgresSearchBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
             new JMS\TranslationBundle\JMSTranslationBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Presta\SitemapBundle\PrestaSitemapBundle(),
@@ -33,6 +34,7 @@ class AppKernel extends Kernel
             new Shopsys\ProductFeed\HeurekaDeliveryBundle\ShopsysProductFeedHeurekaDeliveryBundle(),
             new Shopsys\ProductFeed\ZboziBundle\ShopsysProductFeedZboziBundle(),
             new Shopsys\ProductFeed\GoogleBundle\ShopsysProductFeedGoogleBundle(),
+            new Shopsys\ApiBundle\ShopsysApiBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new Snc\RedisBundle\SncRedisBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
@@ -48,6 +50,7 @@ class AppKernel extends Kernel
             new Shopsys\FrameworkBundle\ShopsysFrameworkBundle(),
             new Shopsys\ReadModelBundle\ShopsysReadModelBundle(), // has to be loaded after ShopsysFrameworkBundle because it overrides Twig `image` function
             new Shopsys\ShopBundle\ShopsysShopBundle(), // must be loaded as last, because translations must overwrite other bundles
+            new FOS\RestBundle\FOSRestBundle(),
         ];
 
         if ($this->getEnvironment() === EnvironmentType::DEVELOPMENT) {

shopsys/project-base

app/AppKernel.php
@@ -19,8 +19,10 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new FM\ElfinderBundle\FMElfinderBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
             new Fp\JsFormValidatorBundle\FpJsFormValidatorBundle(),
             new Intaro\PostgresSearchBundle\IntaroPostgresSearchBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
             new JMS\TranslationBundle\JMSTranslationBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Presta\SitemapBundle\PrestaSitemapBundle(),
@@ -33,6 +35,7 @@ class AppKernel extends Kernel
             new Shopsys\ProductFeed\HeurekaDeliveryBundle\ShopsysProductFeedHeurekaDeliveryBundle(),
             new Shopsys\ProductFeed\ZboziBundle\ShopsysProductFeedZboziBundle(),
             new Shopsys\ProductFeed\GoogleBundle\ShopsysProductFeedGoogleBundle(),
+            new Shopsys\BackendApiBundle\ShopsysBackendApiBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new Snc\RedisBundle\SncRedisBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),