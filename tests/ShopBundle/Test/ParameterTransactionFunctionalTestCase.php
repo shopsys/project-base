vp/shopsys

ProductOnCurrentDomainFacadeCountTest: ParameterValueIds are now dynamically loaded in order to load right id for current locale

project-base/tests/ShopBundle/Test/ParameterTransactionFunctionalTestCase.php
@@ -1,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\ShopBundle\Test;
+
+use Shopsys\FrameworkBundle\Component\Domain\Domain;
+use Shopsys\FrameworkBundle\Model\Product\Parameter\ParameterFacade;
+
+class ParameterTransactionFunctionalTestCase extends TransactionFunctionalTestCase
+{
+    /**
+     * @param string $parameterValueNameId
+     * @return int
+     */
+    protected function getParameterValueId(string $parameterValueNameId): int
+    {
+        /** @var \Shopsys\FrameworkBundle\Component\Domain\Domain $domain */
+        $domain = $this->getContainer()->get(Domain::class);
+
+        /** @var \Shopsys\FrameworkBundle\Model\Product\Parameter\ParameterFacade $parameterFacade */
+        $parameterFacade = $this->getContainer()->get(ParameterFacade::class);
+
+        $firstDomainLocale = $domain->getDomainConfigById(1)->getLocale();
+        $parameterValueTranslatedName = t($parameterValueNameId, [], 'dataFixtures', $firstDomainLocale);
+
+        return $parameterFacade->getParameterValueByValueTextAndLocale($parameterValueTranslatedName, $firstDomainLocale)->getId();
+    }
+}

shopsys/project-base

tests/ShopBundle/Test/ParameterTransactionFunctionalTestCase.php
@@ -1,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\ShopBundle\Test;
+
+use Shopsys\FrameworkBundle\Component\Domain\Domain;
+use Shopsys\FrameworkBundle\Model\Product\Parameter\ParameterFacade;
+
+class ParameterTransactionFunctionalTestCase extends TransactionFunctionalTestCase
+{
+    /**
+     * @param string $parameterValueNameId
+     * @return int
+     */
+    protected function getParameterValueIdForFirstDomain(string $parameterValueNameId): int
+    {
+        /** @var \Shopsys\FrameworkBundle\Component\Domain\Domain $domain */
+        $domain = $this->getContainer()->get(Domain::class);
+
+        /** @var \Shopsys\FrameworkBundle\Model\Product\Parameter\ParameterFacade $parameterFacade */
+        $parameterFacade = $this->getContainer()->get(ParameterFacade::class);
+
+        $firstDomainLocale = $domain->getDomainConfigById(1)->getLocale();
+        $parameterValueTranslatedName = t($parameterValueNameId, [], 'dataFixtures', $firstDomainLocale);
+
+        return $parameterFacade->getParameterValueByValueTextAndLocale($parameterValueTranslatedName, $firstDomainLocale)->getId();
+    }
+}