nicomartinezdev/shopsys

split parameters on each line for too long method calls

project-base/tests/ReadModelBundle/Functional/Image/ImageViewFacadeTest.php
@@ -52,7 +52,10 @@ class ImageViewFacadeTest extends FunctionalTestCase
 
     public function testGetForEntityIds(): void
     {
-        $imageViews = $this->imageViewFacade->getForEntityIds(Product::class, [self::PRODUCT_ID_1, self::PRODUCT_ID_2, self::PRODUCT_ID_3]);
+        $imageViews = $this->imageViewFacade->getForEntityIds(
+            Product::class,
+            [self::PRODUCT_ID_1, self::PRODUCT_ID_2, self::PRODUCT_ID_3]
+        );
 
         $expected = [
             self::PRODUCT_ID_1 => new ImageView(self::PRODUCT_IMAGE_PAIRS[self::PRODUCT_ID_1], 'jpg', 'product', null),

shopsys/project-base

tests/ReadModelBundle/Functional/Image/ImageViewFacadeTest.php
@@ -52,7 +52,10 @@ class ImageViewFacadeTest extends FunctionalTestCase
 
     public function testGetForEntityIds(): void
     {
-        $imageViews = $this->imageViewFacade->getMainImagesByEntityIds(Product::class, [self::PRODUCT_ID_1, self::PRODUCT_ID_2, self::PRODUCT_ID_3]);
+        $imageViews = $this->imageViewFacade->getMainImagesByEntityIds(
+            Product::class,
+            [self::PRODUCT_ID_1, self::PRODUCT_ID_2, self::PRODUCT_ID_3]
+        );
 
         $expected = [
             self::PRODUCT_ID_1 => new ImageView(self::PRODUCT_IMAGE_PAIRS[self::PRODUCT_ID_1], 'jpg', 'product', null),