shopsys/shopsys

refactored error exchange so it now behaves globally
- this way we do not need to wrap each query in useQueryError
- useQueryError was deleted as it is now obsolete
- global error handling only works on operations which have
GraphQL type 'query'

project-base/storefront/components/Pages/Search/SearchProductsWrapper.tsx
@@ -14,7 +14,7 @@ import {
 } from 'helpers/queryParams/queryParamNames';
 import { getProductListSort } from 'helpers/sorting/getProductListSort';
 import { parseProductListSortFromQuery } from 'helpers/sorting/parseProductListSortFromQuery';
-import { useQueryError } from 'hooks/graphQl/useQueryError';
+
 import { useGtmPaginatedProductListViewEvent } from 'hooks/gtm/productList/useGtmPaginatedProductListViewEvent';
 import { useQueryParams } from 'hooks/useQueryParams';
 import { useRouter } from 'next/router';
@@ -32,17 +32,15 @@ export const SearchProductsWrapper: FC<SearchProductsWrapperProps> = ({ containe
     const orderingMode = getProductListSort(parseProductListSortFromQuery(query[SORT_QUERY_PARAMETER_NAME]));
     const parametersFilter = getFilterOptions(parseFilterOptionsFromQuery(query[FILTER_QUERY_PARAMETER_NAME]));
 
-    const [{ data: searchProductsData, fetching }] = useQueryError(
-        useSearchProductsQueryApi({
-            variables: {
-                endCursor: getEndCursor(currentPage),
-                filter: mapParametersFilter(parametersFilter),
-                orderingMode,
-                search: queryString,
-                pageSize: DEFAULT_PAGE_SIZE,
-            },
-        }),
-    );
+    const [{ data: searchProductsData, fetching }] = useSearchProductsQueryApi({
+        variables: {
+            endCursor: getEndCursor(currentPage),
+            filter: mapParametersFilter(parametersFilter),
+            orderingMode,
+            search: queryString,
+            pageSize: DEFAULT_PAGE_SIZE,
+        },
+    });
 
     const searchResultProducts = getMappedProducts(searchProductsData?.products.edges);
 

shopsys/project-base

storefront/components/Pages/Search/SearchProductsWrapper.tsx
@@ -4,7 +4,6 @@ import { getEndCursor } from 'components/Blocks/Product/Filter/helpers/getEndCur
 import { useSearchProductsQueryApi } from 'graphql/generated';
 import { mapParametersFilter } from 'helpers/filterOptions/mapParametersFilter';
 import { getMappedProducts } from 'helpers/mappers/products';
-import { useQueryError } from 'hooks/graphQl/useQueryError';
 import { useGtmPaginatedProductListViewEvent } from 'hooks/gtm/productList/useGtmPaginatedProductListViewEvent';
 import { useQueryParams } from 'hooks/useQueryParams';
 import { RefObject } from 'react';
@@ -17,17 +16,15 @@ type SearchProductsWrapperProps = {
 export const SearchProductsWrapper: FC<SearchProductsWrapperProps> = ({ paginationScrollTargetRef }) => {
     const { currentPage, sort, filter, searchString } = useQueryParams();
 
-    const [{ data: searchProductsData, fetching }] = useQueryError(
-        useSearchProductsQueryApi({
-            variables: {
-                endCursor: getEndCursor(currentPage),
-                filter: mapParametersFilter(filter),
-                orderingMode: sort,
-                search: searchString ?? '',
-                pageSize: DEFAULT_PAGE_SIZE,
-            },
-        }),
-    );
+    const [{ data: searchProductsData, fetching }] = useSearchProductsQueryApi({
+        variables: {
+            endCursor: getEndCursor(currentPage),
+            filter: mapParametersFilter(filter),
+            orderingMode: sort,
+            search: searchString ?? '',
+            pageSize: DEFAULT_PAGE_SIZE,
+        },
+    });
 
     const searchResultProducts = getMappedProducts(searchProductsData?.products.edges);
 