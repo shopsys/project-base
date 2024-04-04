shopsys/shopsys

split useQueryParams into separate files

project-base/storefront/components/Blocks/Product/ProductsList/ProductsListContent.tsx
@@ -5,7 +5,7 @@ import { GtmMessageOriginType } from 'gtm/enums/GtmMessageOriginType';
 import { GtmProductListNameType } from 'gtm/enums/GtmProductListNameType';
 import { useComparison } from 'hooks/productLists/comparison/useComparison';
 import { useWishlist } from 'hooks/productLists/wishlist/useWishlist';
-import { useQueryParams } from 'hooks/useQueryParams';
+import { useCurrentPage } from 'hooks/queryParams/useCurrentPage';
 import dynamic from 'next/dynamic';
 import React, { RefObject } from 'react';
 import { SwipeableHandlers } from 'react-swipeable';
@@ -36,7 +36,7 @@ export const ProductsListContent: FC<ProductsListProps> = ({
     children,
     swipeHandlers,
 }) => {
-    const { currentPage } = useQueryParams();
+    const currentPage = useCurrentPage();
     const { isPopupCompareOpen, toggleProductInComparison, setIsPopupCompareOpen, isProductInComparison } =
         useComparison();
     const { toggleProductInWishlist, isProductInWishlist } = useWishlist();

project-base/storefront/components/Blocks/SortingBar/SortingBar.tsx
@@ -4,7 +4,8 @@ import { DEFAULT_SORT } from 'config/constants';
 import { ProductOrderingModeEnum } from 'graphql/types';
 import { getUrlQueriesWithoutDynamicPageQueries } from 'helpers/parsing/getUrlQueriesWithoutDynamicPageQueries';
 import { twMergeCustom } from 'helpers/twMerge';
-import { useQueryParams } from 'hooks/useQueryParams';
+import { useCurrentSort } from 'hooks/queryParams/useCurrentSort';
+import { useUpdateSort } from 'hooks/queryParams/useUpdateSort';
 import useTranslation from 'next-translate/useTranslation';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
@@ -25,7 +26,8 @@ const DEFAULT_SORT_OPTIONS = [
 export const SortingBar: FC<SortingBarProps> = ({ sorting, totalCount, customSortOptions, className }) => {
     const { t } = useTranslation();
     const router = useRouter();
-    const { sort: sortSelected, updateSort } = useQueryParams();
+    const currentSort = useCurrentSort();
+    const updateSort = useUpdateSort();
     const [isSortMenuOpen, setIsSortMenuOpen] = useState(false);
 
     const asPathWithoutQueryParams = router.asPath.split('?')[0];
@@ -40,7 +42,7 @@ export const SortingBar: FC<SortingBarProps> = ({ sorting, totalCount, customSor
     };
 
     const sortOptions = customSortOptions || DEFAULT_SORT_OPTIONS;
-    const selectedSortOption = sortSelected || sorting || DEFAULT_SORT;
+    const selectedSortOption = currentSort || sorting || DEFAULT_SORT;
 
     return (
         <div

project-base/storefront/pages/brands/[brandSlug].tsx
@@ -32,20 +32,23 @@ import {
 } from 'helpers/queryParamNames';
 import { getServerSidePropsWrapper } from 'helpers/serverSide/getServerSidePropsWrapper';
 import { initServerSideProps } from 'helpers/serverSide/initServerSideProps';
+import { useCurrentFilter } from 'hooks/queryParams/useCurrentFilter';
+import { useCurrentSort } from 'hooks/queryParams/useCurrentSort';
 import { useSeoTitleWithPagination } from 'hooks/seo/useSeoTitleWithPagination';
-import { useQueryParams } from 'hooks/useQueryParams';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import { createClient } from 'urql/createClient';
 
 const BrandDetailPage: NextPage = () => {
     const router = useRouter();
-    const { sort, filter } = useQueryParams();
+    const currentFilter = useCurrentFilter();
+    const currentSort = useCurrentSort();
+
     const [{ data: brandDetailData, fetching }] = useBrandDetailQuery({
         variables: {
             urlSlug: getSlugFromUrl(router.asPath),
-            orderingMode: sort,
-            filter: mapParametersFilter(filter),
+            orderingMode: currentSort,
+            filter: mapParametersFilter(currentFilter),
         },
     });
 
@@ -63,7 +66,7 @@ const BrandDetailPage: NextPage = () => {
             breadcrumbs={brandDetailData?.brand?.breadcrumb}
             description={brandDetailData?.brand?.seoMetaDescription}
             hreflangLinks={brandDetailData?.brand?.hreflangLinks}
-            isFetchingData={!filter && fetching && !brandDetailData}
+            isFetchingData={!currentFilter && fetching && !brandDetailData}
             title={seoTitle}
         >
             {!!brandDetailData?.brand && <BrandDetailContent brand={brandDetailData.brand} />}

shopsys/project-base

storefront/components/Blocks/Product/ProductsList/ProductsListContent.tsx
@@ -5,7 +5,7 @@ import { GtmMessageOriginType } from 'gtm/enums/GtmMessageOriginType';
 import { GtmProductListNameType } from 'gtm/enums/GtmProductListNameType';
 import { useComparison } from 'hooks/productLists/comparison/useComparison';
 import { useWishlist } from 'hooks/productLists/wishlist/useWishlist';
-import { useQueryParams } from 'hooks/useQueryParams';
+import { useCurrentPageQuery } from 'hooks/queryParams/useCurrentPageQuery';
 import dynamic from 'next/dynamic';
 import React, { RefObject } from 'react';
 import { SwipeableHandlers } from 'react-swipeable';
@@ -36,7 +36,7 @@ export const ProductsListContent: FC<ProductsListProps> = ({
     children,
     swipeHandlers,
 }) => {
-    const { currentPage } = useQueryParams();
+    const currentPage = useCurrentPageQuery();
     const { isPopupCompareOpen, toggleProductInComparison, setIsPopupCompareOpen, isProductInComparison } =
         useComparison();
     const { toggleProductInWishlist, isProductInWishlist } = useWishlist();

storefront/components/Blocks/SortingBar/SortingBar.tsx
@@ -4,7 +4,8 @@ import { DEFAULT_SORT } from 'config/constants';
 import { ProductOrderingModeEnum } from 'graphql/types';
 import { getUrlQueriesWithoutDynamicPageQueries } from 'helpers/parsing/getUrlQueriesWithoutDynamicPageQueries';
 import { twMergeCustom } from 'helpers/twMerge';
-import { useQueryParams } from 'hooks/useQueryParams';
+import { useCurrentSortQuery } from 'hooks/queryParams/useCurrentSortQuery';
+import { useUpdateSortQuery } from 'hooks/queryParams/useUpdateSortQuery';
 import useTranslation from 'next-translate/useTranslation';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
@@ -25,7 +26,8 @@ const DEFAULT_SORT_OPTIONS = [
 export const SortingBar: FC<SortingBarProps> = ({ sorting, totalCount, customSortOptions, className }) => {
     const { t } = useTranslation();
     const router = useRouter();
-    const { sort: sortSelected, updateSort } = useQueryParams();
+    const currentSort = useCurrentSortQuery();
+    const updateSort = useUpdateSortQuery();
     const [isSortMenuOpen, setIsSortMenuOpen] = useState(false);
 
     const asPathWithoutQueryParams = router.asPath.split('?')[0];
@@ -40,7 +42,7 @@ export const SortingBar: FC<SortingBarProps> = ({ sorting, totalCount, customSor
     };
 
     const sortOptions = customSortOptions || DEFAULT_SORT_OPTIONS;
-    const selectedSortOption = sortSelected || sorting || DEFAULT_SORT;
+    const selectedSortOption = currentSort || sorting || DEFAULT_SORT;
 
     return (
         <div

storefront/pages/brands/[brandSlug].tsx
@@ -32,20 +32,23 @@ import {
 } from 'helpers/queryParamNames';
 import { getServerSidePropsWrapper } from 'helpers/serverSide/getServerSidePropsWrapper';
 import { initServerSideProps } from 'helpers/serverSide/initServerSideProps';
+import { useCurrentFilterQuery } from 'hooks/queryParams/useCurrentFilterQuery';
+import { useCurrentSortQuery } from 'hooks/queryParams/useCurrentSortQuery';
 import { useSeoTitleWithPagination } from 'hooks/seo/useSeoTitleWithPagination';
-import { useQueryParams } from 'hooks/useQueryParams';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import { createClient } from 'urql/createClient';
 
 const BrandDetailPage: NextPage = () => {
     const router = useRouter();
-    const { sort, filter } = useQueryParams();
+    const currentFilter = useCurrentFilterQuery();
+    const currentSort = useCurrentSortQuery();
+
     const [{ data: brandDetailData, fetching }] = useBrandDetailQuery({
         variables: {
             urlSlug: getSlugFromUrl(router.asPath),
-            orderingMode: sort,
-            filter: mapParametersFilter(filter),
+            orderingMode: currentSort,
+            filter: mapParametersFilter(currentFilter),
         },
     });
 
@@ -63,7 +66,7 @@ const BrandDetailPage: NextPage = () => {
             breadcrumbs={brandDetailData?.brand?.breadcrumb}
             description={brandDetailData?.brand?.seoMetaDescription}
             hreflangLinks={brandDetailData?.brand?.hreflangLinks}
-            isFetchingData={!filter && fetching && !brandDetailData}
+            isFetchingData={!currentFilter && fetching && !brandDetailData}
             title={seoTitle}
         >
             {!!brandDetailData?.brand && <BrandDetailContent brand={brandDetailData.brand} />}