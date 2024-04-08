shopsys/shopsys

string is now used instead of ProductListTypeEnum
- it is not possible to extend PHP enums

project-base/app/tests/FrontendApiBundle/Functional/Product/ProductList/ProductListTypesDataProvider.php
@@ -5,7 +5,8 @@ declare(strict_types=1);
 namespace Tests\FrontendApiBundle\Functional\Product\ProductList;
 
 use Iterator;
-use Shopsys\FrameworkBundle\Model\Product\List\ProductListTypeEnum;
+use Shopsys\FrameworkBundle\Component\Reflection\ReflectionHelper;
+use Shopsys\FrameworkBundle\Model\Product\List\ProductListType;
 
 class ProductListTypesDataProvider
 {
@@ -14,7 +15,7 @@ class ProductListTypesDataProvider
      */
     public static function getProductListTypes(): Iterator
     {
-        foreach (ProductListTypeEnum::cases() as $productListType) {
+        foreach (ReflectionHelper::getAllPublicClassConstants(ProductListType::class) as $productListType) {
             yield [$productListType];
         }
     }

shopsys/project-base

app/tests/FrontendApiBundle/Functional/Product/ProductList/ProductListTypesDataProvider.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Tests\FrontendApiBundle\Functional\Product\ProductList;
 
 use Iterator;
+use Shopsys\FrameworkBundle\Component\Reflection\ReflectionHelper;
 use Shopsys\FrameworkBundle\Model\Product\List\ProductListTypeEnum;
 
 class ProductListTypesDataProvider
@@ -14,7 +15,7 @@ class ProductListTypesDataProvider
      */
     public static function getProductListTypes(): Iterator
     {
-        foreach (ProductListTypeEnum::cases() as $productListType) {
+        foreach (ReflectionHelper::getAllPublicClassConstants(ProductListTypeEnum::class) as $productListType) {
             yield [$productListType];
         }
     }