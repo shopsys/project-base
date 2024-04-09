shopsys/shopsys

add Styleguide page

project-base/storefront/components/Pages/Styleguide/StyleguideForms.tsx
@@ -1,0 +1,77 @@
+import { StyleguideSection } from './StyleguideElements';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { SubmitButton } from 'components/Forms/Button/SubmitButton';
+import { Form } from 'components/Forms/Form/Form';
+import { FormLine } from 'components/Forms/Lib/FormLine';
+import { TextInputControlled } from 'components/Forms/TextInput/TextInputControlled';
+import { useShopsysForm } from 'hooks/forms/useShopsysForm';
+import { Translate } from 'next-translate';
+import useTranslation from 'next-translate/useTranslation';
+import { FormProvider } from 'react-hook-form';
+import * as Yup from 'yup';
+
+export const StyleguideForms: FC = () => {
+    return (
+        <StyleguideSection className="flex flex-col gap-3" title="Forms">
+            <StyleguideFormExample />
+        </StyleguideSection>
+    );
+};
+
+export const StyleguideFormExample: FC = () => {
+    const { t } = useTranslation();
+    const formProviderMethods = useShopsysForm(getStyleguideExampleFormResolver(t), {
+        optionalValue: '',
+        requiredValue: '',
+    });
+
+    const onValidForm = () => {
+        // eslint-disable-next-line no-alert
+        alert('Valid form handler');
+    };
+
+    const onInvalidForm = () => {
+        // eslint-disable-next-line no-alert
+        alert('Invalid form handler');
+    };
+
+    return (
+        <FormProvider {...formProviderMethods}>
+            <Form onSubmit={formProviderMethods.handleSubmit(onValidForm, onInvalidForm)}>
+                <TextInputControlled
+                    control={formProviderMethods.control}
+                    formName="example-form"
+                    name="optionalValue"
+                    render={(optionalValue) => <FormLine bottomGap>{optionalValue}</FormLine>}
+                    textInputProps={{
+                        label: 'Optional Value',
+                        type: 'text',
+                    }}
+                />
+                <TextInputControlled
+                    control={formProviderMethods.control}
+                    formName="example-form"
+                    name="requiredValue"
+                    render={(requiredInput) => <FormLine>{requiredInput}</FormLine>}
+                    textInputProps={{
+                        label: 'Required value',
+                        required: true,
+                        type: 'text',
+                    }}
+                />
+                <div className="mt-3">
+                    <SubmitButton>Submit form</SubmitButton>
+                </div>
+            </Form>
+        </FormProvider>
+    );
+};
+
+const getStyleguideExampleFormResolver = (t: Translate) => {
+    return yupResolver(
+        Yup.object().shape<Record<keyof { optionalValue: string; requiredValue: string }, any>>({
+            optionalValue: Yup.string(),
+            requiredValue: Yup.string().required(t('This field is required')),
+        }),
+    );
+};

project-base/storefront/components/Pages/Styleguide/StyleguideRadiogroup.tsx
@@ -1,0 +1,60 @@
+import { StyleguideSection } from './StyleguideElements';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { FormLine } from 'components/Forms/Lib/FormLine';
+import { RadiobuttonGroup } from 'components/Forms/Radiobutton/RadiobuttonGroup';
+import { useShopsysForm } from 'hooks/forms/useShopsysForm';
+import { FormProvider } from 'react-hook-form';
+import * as Yup from 'yup';
+
+const getStyleguideExampleFormResolver = () =>
+    yupResolver(
+        Yup.object().shape<Record<keyof { country: 'cz' | 'de' }, any>>({
+            country: Yup.string().oneOf(['cz', 'de']),
+        }),
+    );
+
+export const StyleguideRadiogroup: FC = () => {
+    const formProviderMethods = useShopsysForm(getStyleguideExampleFormResolver(), {
+        country: 'cz',
+    });
+
+    const formMeta = {
+        formName: 'contact-information-form',
+        messages: {
+            error: 'Could not create order',
+        },
+        fields: {
+            country: {
+                name: 'country' as const,
+                label: 'Country',
+            },
+        },
+    };
+
+    return (
+        <StyleguideSection className="flex flex-col gap-3" title="RadioGroup">
+            <FormProvider {...formProviderMethods}>
+                <RadiobuttonGroup
+                    control={formProviderMethods.control}
+                    formName={formMeta.formName}
+                    name={formMeta.fields.country.name}
+                    radiobuttons={[
+                        {
+                            label: 'Czechia',
+                            value: 'cz',
+                        },
+                        {
+                            label: 'Germany',
+                            value: 'de',
+                        },
+                    ]}
+                    render={(radiobutton, key) => (
+                        <FormLine key={key} bottomGap className="w-full flex-none lg:w-1/2">
+                            {radiobutton}
+                        </FormLine>
+                    )}
+                />
+            </FormProvider>
+        </StyleguideSection>
+    );
+};

shopsys/project-base

storefront/components/Pages/Styleguide/StyleguideForms.tsx
@@ -1,0 +1,77 @@
+import { StyleguideSection } from './StyleguideElements';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { SubmitButton } from 'components/Forms/Button/SubmitButton';
+import { Form } from 'components/Forms/Form/Form';
+import { FormLine } from 'components/Forms/Lib/FormLine';
+import { TextInputControlled } from 'components/Forms/TextInput/TextInputControlled';
+import { Translate } from 'next-translate';
+import useTranslation from 'next-translate/useTranslation';
+import { FormProvider } from 'react-hook-form';
+import { useShopsysForm } from 'utils/forms/useShopsysForm';
+import * as Yup from 'yup';
+
+export const StyleguideForms: FC = () => {
+    return (
+        <StyleguideSection className="flex flex-col gap-3" title="Forms">
+            <StyleguideFormExample />
+        </StyleguideSection>
+    );
+};
+
+export const StyleguideFormExample: FC = () => {
+    const { t } = useTranslation();
+    const formProviderMethods = useShopsysForm(getStyleguideExampleFormResolver(t), {
+        optionalValue: '',
+        requiredValue: '',
+    });
+
+    const onValidForm = () => {
+        // eslint-disable-next-line no-alert
+        alert('Valid form handler');
+    };
+
+    const onInvalidForm = () => {
+        // eslint-disable-next-line no-alert
+        alert('Invalid form handler');
+    };
+
+    return (
+        <FormProvider {...formProviderMethods}>
+            <Form onSubmit={formProviderMethods.handleSubmit(onValidForm, onInvalidForm)}>
+                <TextInputControlled
+                    control={formProviderMethods.control}
+                    formName="example-form"
+                    name="optionalValue"
+                    render={(optionalValue) => <FormLine bottomGap>{optionalValue}</FormLine>}
+                    textInputProps={{
+                        label: 'Optional Value',
+                        type: 'text',
+                    }}
+                />
+                <TextInputControlled
+                    control={formProviderMethods.control}
+                    formName="example-form"
+                    name="requiredValue"
+                    render={(requiredInput) => <FormLine>{requiredInput}</FormLine>}
+                    textInputProps={{
+                        label: 'Required value',
+                        required: true,
+                        type: 'text',
+                    }}
+                />
+                <div className="mt-3">
+                    <SubmitButton>Submit form</SubmitButton>
+                </div>
+            </Form>
+        </FormProvider>
+    );
+};
+
+const getStyleguideExampleFormResolver = (t: Translate) => {
+    return yupResolver(
+        Yup.object().shape<Record<keyof { optionalValue: string; requiredValue: string }, any>>({
+            optionalValue: Yup.string(),
+            requiredValue: Yup.string().required(t('This field is required')),
+        }),
+    );
+};

storefront/components/Pages/Styleguide/StyleguideRadiogroup.tsx
@@ -1,0 +1,60 @@
+import { StyleguideSection } from './StyleguideElements';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { FormLine } from 'components/Forms/Lib/FormLine';
+import { RadiobuttonGroup } from 'components/Forms/Radiobutton/RadiobuttonGroup';
+import { FormProvider } from 'react-hook-form';
+import { useShopsysForm } from 'utils/forms/useShopsysForm';
+import * as Yup from 'yup';
+
+const getStyleguideExampleFormResolver = () =>
+    yupResolver(
+        Yup.object().shape<Record<keyof { country: 'cz' | 'de' }, any>>({
+            country: Yup.string().oneOf(['cz', 'de']),
+        }),
+    );
+
+export const StyleguideRadiogroup: FC = () => {
+    const formProviderMethods = useShopsysForm(getStyleguideExampleFormResolver(), {
+        country: 'cz',
+    });
+
+    const formMeta = {
+        formName: 'contact-information-form',
+        messages: {
+            error: 'Could not create order',
+        },
+        fields: {
+            country: {
+                name: 'country' as const,
+                label: 'Country',
+            },
+        },
+    };
+
+    return (
+        <StyleguideSection className="flex flex-col gap-3" title="RadioGroup">
+            <FormProvider {...formProviderMethods}>
+                <RadiobuttonGroup
+                    control={formProviderMethods.control}
+                    formName={formMeta.formName}
+                    name={formMeta.fields.country.name}
+                    radiobuttons={[
+                        {
+                            label: 'Czechia',
+                            value: 'cz',
+                        },
+                        {
+                            label: 'Germany',
+                            value: 'de',
+                        },
+                    ]}
+                    render={(radiobutton, key) => (
+                        <FormLine key={key} bottomGap className="w-full flex-none lg:w-1/2">
+                            {radiobutton}
+                        </FormLine>
+                    )}
+                />
+            </FormProvider>
+        </StyleguideSection>
+    );
+};