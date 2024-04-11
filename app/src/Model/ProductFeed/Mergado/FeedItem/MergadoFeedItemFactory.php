shopsys/shopsys

string is now used instead of AvailabilityStatusEnum
- it is not possible to extend PHP enums

project-base/app/src/Model/ProductFeed/Mergado/FeedItem/MergadoFeedItemFactory.php
@@ -14,7 +14,7 @@ use Shopsys\FrameworkBundle\Component\Image\Exception\ImageNotFoundException;
 use Shopsys\FrameworkBundle\Component\Image\ImageUrlWithSizeHelper;
 use Shopsys\FrameworkBundle\Model\Pricing\Currency\CurrencyFacade;
 use Shopsys\FrameworkBundle\Model\Pricing\Group\PricingGroupSettingFacade;
-use Shopsys\FrameworkBundle\Model\Product\Availability\AvailabilityStatusEnum;
+use Shopsys\FrameworkBundle\Model\Product\Availability\AvailabilityStatus;
 use Shopsys\FrameworkBundle\Model\Product\Availability\ProductAvailabilityFacade;
 use Shopsys\FrameworkBundle\Model\Product\Collection\ProductParametersBatchLoader;
 use Shopsys\FrameworkBundle\Model\Product\Collection\ProductUrlsBatchLoader;
@@ -167,13 +167,13 @@ class MergadoFeedItemFactory
     }
 
     /**
-     * @param \Shopsys\FrameworkBundle\Model\Product\Availability\AvailabilityStatusEnum $availabilityStatus
+     * @param string $availabilityStatus
      * @return string
      */
-    private function mapStockStatusToAvailability(AvailabilityStatusEnum $availabilityStatus): string
+    private function mapStockStatusToAvailability(string $availabilityStatus): string
     {
         return match ($availabilityStatus) {
-            AvailabilityStatusEnum::InStock => 'in stock',
+            AvailabilityStatus::IN_STOCK => 'in stock',
             default => 'out of stock'
         };
     }

project-base/app/tests/FrontendApiBundle/Functional/Cart/RetrieveCartTest.php
@@ -9,7 +9,7 @@ use App\DataFixtures\Demo\ProductDataFixture;
 use App\DataFixtures\Demo\VatDataFixture;
 use App\Model\Product\Product;
 use Shopsys\FrameworkBundle\Component\Translation\Translator;
-use Shopsys\FrameworkBundle\Model\Product\Availability\AvailabilityStatusEnum;
+use Shopsys\FrameworkBundle\Model\Product\Availability\AvailabilityStatus;
 use Shopsys\FrameworkBundle\Model\Product\Availability\ProductAvailabilityFacade;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Tests\FrontendApiBundle\Test\GraphQlTestCase;
@@ -350,7 +350,7 @@ class RetrieveCartTest extends GraphQlTestCase
             ],
             'availability' => [
                 'name' => t('In stock', [], Translator::DATA_FIXTURES_TRANSLATION_DOMAIN, $firstDomainLocale),
-                'status' => AvailabilityStatusEnum::InStock->name,
+                'status' => AvailabilityStatus::IN_STOCK,
             ],
             'stockQuantity' => 2700,
             'categories' => [
@@ -544,13 +544,13 @@ class RetrieveCartTest extends GraphQlTestCase
                         'name' => 'Ostrava',
                     ],
                     'availabilityInformation' => t('Available immediately', [], Translator::DEFAULT_TRANSLATION_DOMAIN, $this->getFirstDomainLocale()),
-                    'availabilityStatus' => AvailabilityStatusEnum::InStock->name,
+                    'availabilityStatus' => AvailabilityStatus::IN_STOCK,
                 ], [
                     'store' => [
                         'name' => 'Pardubice',
                     ],
                     'availabilityInformation' => t('{0,1} Available in one week|[2,Inf] Available in %count% weeks', ['%count%' => 1], Translator::DEFAULT_TRANSLATION_DOMAIN, $this->getFirstDomainLocale()),
-                    'availabilityStatus' => AvailabilityStatusEnum::InStock->name,
+                    'availabilityStatus' => AvailabilityStatus::IN_STOCK,
                 ],
             ],
             'availableStoresCount' => 1,

project-base/app/tests/FrontendApiBundle/Functional/Product/ProductTest.php
@@ -9,7 +9,7 @@ use App\DataFixtures\Demo\ProductDataFixture;
 use App\DataFixtures\Demo\VatDataFixture;
 use App\Model\Product\Product;
 use Shopsys\FrameworkBundle\Component\Translation\Translator;
-use Shopsys\FrameworkBundle\Model\Product\Availability\AvailabilityStatusEnum;
+use Shopsys\FrameworkBundle\Model\Product\Availability\AvailabilityStatus;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Tests\FrontendApiBundle\Test\GraphQlTestCase;
 
@@ -194,7 +194,7 @@ class ProductTest extends GraphQlTestCase
                     ],
                     'availability' => [
                         'name' => t('In stock', [], Translator::DATA_FIXTURES_TRANSLATION_DOMAIN, $firstDomainLocale),
-                        'status' => AvailabilityStatusEnum::InStock->name,
+                        'status' => AvailabilityStatus::IN_STOCK,
                     ],
                     'stockQuantity' => 2700,
                     'categories' => [
@@ -388,13 +388,13 @@ class ProductTest extends GraphQlTestCase
                                 'name' => 'Ostrava',
                             ],
                             'availabilityInformation' => t('Available immediately', [], Translator::DEFAULT_TRANSLATION_DOMAIN, $firstDomainLocale),
-                            'availabilityStatus' => AvailabilityStatusEnum::InStock->name,
+                            'availabilityStatus' => AvailabilityStatus::IN_STOCK,
                         ], [
                             'store' => [
                                 'name' => 'Pardubice',
                             ],
                             'availabilityInformation' => t('{0,1} Available in one week|[2,Inf] Available in %count% weeks', ['%count%' => 1], Translator::DEFAULT_TRANSLATION_DOMAIN, $firstDomainLocale),
-                            'availabilityStatus' => AvailabilityStatusEnum::InStock->name,
+                            'availabilityStatus' => AvailabilityStatus::IN_STOCK,
                         ],
                     ],
                     'availableStoresCount' => 1,

project-base/app/tests/FrontendApiBundle/Functional/Product/ProductsTest.php
@@ -6,7 +6,7 @@ namespace Tests\FrontendApiBundle\Functional\Product;
 
 use App\DataFixtures\Demo\VatDataFixture;
 use Shopsys\FrameworkBundle\Component\Translation\Translator;
-use Shopsys\FrameworkBundle\Model\Product\Availability\AvailabilityStatusEnum;
+use Shopsys\FrameworkBundle\Model\Product\Availability\AvailabilityStatus;
 
 class ProductsTest extends ProductsGraphQlTestCase
 {
@@ -148,7 +148,7 @@ class ProductsTest extends ProductsGraphQlTestCase
                 ],
                 'availability' => [
                     'name' => t('In stock', [], Translator::DATA_FIXTURES_TRANSLATION_DOMAIN, $firstDomainLocale),
-                    'status' => AvailabilityStatusEnum::InStock->name,
+                    'status' => AvailabilityStatus::IN_STOCK,
                 ],
                 'stockQuantity' => 900,
                 'categories' => [

shopsys/project-base

app/src/Model/ProductFeed/Mergado/FeedItem/MergadoFeedItemFactory.php
@@ -167,13 +167,13 @@ class MergadoFeedItemFactory
     }
 
     /**
-     * @param \Shopsys\FrameworkBundle\Model\Product\Availability\AvailabilityStatusEnum $availabilityStatus
+     * @param string $availabilityStatus
      * @return string
      */
-    private function mapStockStatusToAvailability(AvailabilityStatusEnum $availabilityStatus): string
+    private function mapStockStatusToAvailability(string $availabilityStatus): string
     {
         return match ($availabilityStatus) {
-            AvailabilityStatusEnum::InStock => 'in stock',
+            AvailabilityStatusEnum::IN_STOCK => 'in stock',
             default => 'out of stock'
         };
     }

app/tests/FrontendApiBundle/Functional/Cart/RetrieveCartTest.php
@@ -348,7 +348,7 @@ class RetrieveCartTest extends GraphQlTestCase
             ],
             'availability' => [
                 'name' => t('In stock', [], Translator::DATA_FIXTURES_TRANSLATION_DOMAIN, $firstDomainLocale),
-                'status' => AvailabilityStatusEnum::InStock->name,
+                'status' => AvailabilityStatusEnum::IN_STOCK,
             ],
             'stockQuantity' => 2700,
             'categories' => [
@@ -541,13 +541,13 @@ class RetrieveCartTest extends GraphQlTestCase
                         'name' => 'Ostrava',
                     ],
                     'availabilityInformation' => t('Available immediately', [], Translator::DEFAULT_TRANSLATION_DOMAIN, $this->getFirstDomainLocale()),
-                    'availabilityStatus' => AvailabilityStatusEnum::InStock->name,
+                    'availabilityStatus' => AvailabilityStatusEnum::IN_STOCK,
                 ], [
                     'store' => [
                         'name' => 'Pardubice',
                     ],
                     'availabilityInformation' => t('{0,1} Available in one week|[2,Inf] Available in %count% weeks', ['%count%' => 1], Translator::DEFAULT_TRANSLATION_DOMAIN, $this->getFirstDomainLocale()),
-                    'availabilityStatus' => AvailabilityStatusEnum::InStock->name,
+                    'availabilityStatus' => AvailabilityStatusEnum::IN_STOCK,
                 ],
             ],
             'availableStoresCount' => 1,

app/tests/FrontendApiBundle/Functional/Product/ProductTest.php
@@ -192,7 +192,7 @@ class ProductTest extends GraphQlTestCase
                     ],
                     'availability' => [
                         'name' => t('In stock', [], Translator::DATA_FIXTURES_TRANSLATION_DOMAIN, $firstDomainLocale),
-                        'status' => AvailabilityStatusEnum::InStock->name,
+                        'status' => AvailabilityStatusEnum::IN_STOCK,
                     ],
                     'stockQuantity' => 2700,
                     'categories' => [
@@ -385,13 +385,13 @@ class ProductTest extends GraphQlTestCase
                                 'name' => 'Ostrava',
                             ],
                             'availabilityInformation' => t('Available immediately', [], Translator::DEFAULT_TRANSLATION_DOMAIN, $firstDomainLocale),
-                            'availabilityStatus' => AvailabilityStatusEnum::InStock->name,
+                            'availabilityStatus' => AvailabilityStatusEnum::IN_STOCK,
                         ], [
                             'store' => [
                                 'name' => 'Pardubice',
                             ],
                             'availabilityInformation' => t('{0,1} Available in one week|[2,Inf] Available in %count% weeks', ['%count%' => 1], Translator::DEFAULT_TRANSLATION_DOMAIN, $firstDomainLocale),
-                            'availabilityStatus' => AvailabilityStatusEnum::InStock->name,
+                            'availabilityStatus' => AvailabilityStatusEnum::IN_STOCK,
                         ],
                     ],
                     'availableStoresCount' => 1,

app/tests/FrontendApiBundle/Functional/Product/ProductsTest.php
@@ -147,7 +147,7 @@ class ProductsTest extends ProductsGraphQlTestCase
                 ],
                 'availability' => [
                     'name' => t('In stock', [], Translator::DATA_FIXTURES_TRANSLATION_DOMAIN, $firstDomainLocale),
-                    'status' => AvailabilityStatusEnum::InStock->name,
+                    'status' => AvailabilityStatusEnum::IN_STOCK,
                 ],
                 'stockQuantity' => 900,
                 'categories' => [