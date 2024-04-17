shopsys/shopsys

refactor search page and filters
- now we have splitted logic for fetching product search and the rest
- in previous solution changes made to filters were causing both queries (main search and product search) to be fired at the same times that was causing problems
- improved behavior on search by adding skeletons

project-base/storefront/pages/search.tsx
@@ -1,48 +1,24 @@
 import { MetaRobots } from 'components/Basic/Head/MetaRobots';
-import { LastVisitedProducts } from 'components/Blocks/Product/LastVisitedProducts/LastVisitedProducts';
 import { CommonLayout } from 'components/Layout/CommonLayout';
-import { Webline } from 'components/Layout/Webline/Webline';
-import { SearchContent } from 'components/Pages/Search/SearchContent';
+import { SearchPageContent } from 'components/Pages/Search/SearchPageContent';
 import { useDomainConfig } from 'components/providers/DomainConfigProvider';
-import { DEFAULT_PAGE_SIZE } from 'config/constants';
-import {
-    BreadcrumbFragmentApi,
-    SearchProductsQueryVariablesApi,
-    SearchQueryVariablesApi,
-    useSearchQueryApi,
-} from 'graphql/generated';
+import { BreadcrumbFragmentApi, SearchProductsQueryVariablesApi, SearchQueryVariablesApi } from 'graphql/generated';
 import { useGtmStaticPageViewEvent } from 'gtm/helpers/eventFactories';
 import { useGtmPageViewEvent } from 'gtm/hooks/useGtmPageViewEvent';
 import { GtmPageType } from 'gtm/types/enums';
-import { mapParametersFilter } from 'helpers/filterOptions/mapParametersFilter';
 import { getInternationalizedStaticUrls } from 'helpers/getInternationalizedStaticUrls';
 import { getRedirectWithOffsetPage } from 'helpers/loadMore';
 import { getNumberFromUrlQuery, getSlugFromServerSideUrl } from 'helpers/parsing/urlParsing';
 import { LOAD_MORE_QUERY_PARAMETER_NAME, PAGE_QUERY_PARAMETER_NAME } from 'helpers/queryParamNames';
 import { getServerSidePropsWrapper } from 'helpers/serverSide/getServerSidePropsWrapper';
 import { initServerSideProps, ServerSidePropsType } from 'helpers/serverSide/initServerSideProps';
-import { useSeoTitleWithPagination } from 'hooks/seo/useSeoTitleWithPagination';
 import { useQueryParams } from 'hooks/useQueryParams';
 import useTranslation from 'next-translate/useTranslation';
-import { usePersistStore } from 'store/usePersistStore';
 
 const SearchPage: FC<ServerSidePropsType> = () => {
     const { t } = useTranslation();
     const { url } = useDomainConfig();
-    const { sort, filter, searchString, currentLoadMore } = useQueryParams();
-    const userIdentifier = usePersistStore((state) => state.userId)!;
-
-    const [{ data: searchData, fetching }] = useSearchQueryApi({
-        variables: {
-            search: searchString!,
-            orderingMode: sort,
-            filter: mapParametersFilter(filter),
-            pageSize: DEFAULT_PAGE_SIZE * (currentLoadMore + 1),
-            isAutocomplete: false,
-            userIdentifier,
-        },
-        pause: !searchString,
-    });
+    const { searchString } = useQueryParams();
 
     const [searchUrl] = getInternationalizedStaticUrls(['/search'], url);
     const breadcrumbs: BreadcrumbFragmentApi[] = [{ __typename: 'Link', name: t('Search'), slug: searchUrl }];
@@ -50,22 +26,12 @@ const SearchPage: FC<ServerSidePropsType> = () => {
     const gtmStaticPageViewEvent = useGtmStaticPageViewEvent(GtmPageType.search_results, breadcrumbs);
     useGtmPageViewEvent(gtmStaticPageViewEvent);
 
-    const title = useSeoTitleWithPagination(searchData?.productsSearch.totalCount, t('Search'));
-
     return (
         <>
             <MetaRobots content="noindex, nofollow" />
-            <CommonLayout breadcrumbs={breadcrumbs} title={title}>
-                <Webline>
-                    {searchString ? (
-                        <SearchContent fetching={fetching} searchResults={searchData} />
-                    ) : (
-                        <div className="mb-5 p-12 text-center">
-                            <strong>{t('There are no results as you have searched with an empty query...')}</strong>
-                        </div>
-                    )}
-                </Webline>
-                <LastVisitedProducts />
+
+            <CommonLayout breadcrumbs={breadcrumbs} title={t('Search')}>
+                <SearchPageContent key={searchString} />
             </CommonLayout>
         </>
     );

shopsys/project-base

storefront/pages/search.tsx
@@ -1,28 +1,19 @@
 import { MetaRobots } from 'components/Basic/Head/MetaRobots';
-import { LastVisitedProducts } from 'components/Blocks/Product/LastVisitedProducts/LastVisitedProducts';
 import { CommonLayout } from 'components/Layout/CommonLayout';
-import { Webline } from 'components/Layout/Webline/Webline';
-import { SearchContent } from 'components/Pages/Search/SearchContent';
+import { SearchPageContent } from 'components/Pages/Search/SearchPageContent';
 import { useDomainConfig } from 'components/providers/DomainConfigProvider';
-import { DEFAULT_PAGE_SIZE } from 'config/constants';
 import { TypeBreadcrumbFragment } from 'graphql/requests/breadcrumbs/fragments/BreadcrumbFragment.generated';
 import { TypeSearchProductsQueryVariables } from 'graphql/requests/products/queries/SearchProductsQuery.generated';
-import { useSearchQuery, TypeSearchQueryVariables } from 'graphql/requests/search/queries/SearchQuery.generated';
+import { TypeSearchQueryVariables } from 'graphql/requests/search/queries/SearchQuery.generated';
 import { GtmPageType } from 'gtm/enums/GtmPageType';
 import { useGtmStaticPageViewEvent } from 'gtm/factories/useGtmStaticPageViewEvent';
 import { useGtmPageViewEvent } from 'gtm/utils/pageViewEvents/useGtmPageViewEvent';
 import useTranslation from 'next-translate/useTranslation';
-import { usePersistStore } from 'store/usePersistStore';
-import { mapParametersFilter } from 'utils/filterOptions/mapParametersFilter';
 import { getRedirectWithOffsetPage } from 'utils/loadMore/getRedirectWithOffsetPage';
 import { getNumberFromUrlQuery } from 'utils/parsing/getNumberFromUrlQuery';
 import { getSlugFromServerSideUrl } from 'utils/parsing/getSlugFromServerSideUrl';
 import { LOAD_MORE_QUERY_PARAMETER_NAME, PAGE_QUERY_PARAMETER_NAME } from 'utils/queryParamNames';
-import { useCurrentFilterQuery } from 'utils/queryParams/useCurrentFilterQuery';
-import { useCurrentLoadMoreQuery } from 'utils/queryParams/useCurrentLoadMoreQuery';
 import { useCurrentSearchStringQuery } from 'utils/queryParams/useCurrentSearchStringQuery';
-import { useCurrentSortQuery } from 'utils/queryParams/useCurrentSortQuery';
-import { useSeoTitleWithPagination } from 'utils/seo/useSeoTitleWithPagination';
 import { getServerSidePropsWrapper } from 'utils/serverSide/getServerSidePropsWrapper';
 import { initServerSideProps, ServerSidePropsType } from 'utils/serverSide/initServerSideProps';
 import { getInternationalizedStaticUrls } from 'utils/staticUrls/getInternationalizedStaticUrls';
@@ -30,23 +21,7 @@ import { getInternationalizedStaticUrls } from 'utils/staticUrls/getInternationa
 const SearchPage: FC<ServerSidePropsType> = () => {
     const { t } = useTranslation();
     const { url } = useDomainConfig();
-    const currentFilter = useCurrentFilterQuery();
-    const sort = useCurrentSortQuery();
     const currentSearchString = useCurrentSearchStringQuery();
-    const currentLoadMore = useCurrentLoadMoreQuery();
-    const userIdentifier = usePersistStore((state) => state.userId)!;
-
-    const [{ data: searchData, fetching }] = useSearchQuery({
-        variables: {
-            search: currentSearchString!,
-            orderingMode: sort,
-            filter: mapParametersFilter(currentFilter),
-            pageSize: DEFAULT_PAGE_SIZE * (currentLoadMore + 1),
-            isAutocomplete: false,
-            userIdentifier,
-        },
-        pause: !currentSearchString,
-    });
 
     const [searchUrl] = getInternationalizedStaticUrls(['/search'], url);
     const breadcrumbs: TypeBreadcrumbFragment[] = [{ __typename: 'Link', name: t('Search'), slug: searchUrl }];
@@ -54,22 +29,12 @@ const SearchPage: FC<ServerSidePropsType> = () => {
     const gtmStaticPageViewEvent = useGtmStaticPageViewEvent(GtmPageType.search_results, breadcrumbs);
     useGtmPageViewEvent(gtmStaticPageViewEvent);
 
-    const title = useSeoTitleWithPagination(searchData?.productsSearch.totalCount, t('Search'));
-
     return (
         <>
             <MetaRobots content="noindex, nofollow" />
-            <CommonLayout breadcrumbs={breadcrumbs} title={title}>
-                <Webline>
-                    {currentSearchString ? (
-                        <SearchContent fetching={fetching} searchResults={searchData} />
-                    ) : (
-                        <div className="mb-5 p-12 text-center">
-                            <strong>{t('There are no results as you have searched with an empty query...')}</strong>
-                        </div>
-                    )}
-                </Webline>
-                <LastVisitedProducts />
+
+            <CommonLayout breadcrumbs={breadcrumbs} title={t('Search')}>
+                <SearchPageContent key={currentSearchString} />
             </CommonLayout>
         </>
     );