sebaholesz/shopsys

fix Persist store hydration
- in order to make userId works (have it accessible during app initialization) we needed to amend stopping hydration of Persist store
- this means we need to manually handle states for both SSR and client side in places where it's needed for app initialization, otherwise React will complain about not matching UI from SSR with client side rendered one

project-base/storefront/hooks/useSetInitialStoreValues.ts
@@ -1,3 +1,4 @@
+import { isClient } from 'helpers/isClient';
 import { ServerSidePropsType } from 'helpers/serverSide/initServerSideProps';
 import { useRef } from 'react';
 import { CookiesStoreState, useCookiesStore } from 'store/useCookiesStore';
@@ -7,6 +8,7 @@ import { v4 as uuidV4 } from 'uuid';
 export const useSetInitialStoreValues = ({ cookiesStore }: ServerSidePropsType) => {
     const isStoreSet = useRef(false);
 
+    const userId = usePersistStore((store) => store.userId);
     const updateUserId = usePersistStore((store) => store.updateUserId);
     const setCookieStoreValue = useCookiesStore((state) => state.setCookiesStoreState);
 
@@ -17,17 +19,17 @@ export const useSetInitialStoreValues = ({ cookiesStore }: ServerSidePropsType) 
     };
 
     const setPersistStoreValues = () => {
-        // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-        usePersistStore.persist?.onFinishHydration(({ userId }) => {
-            if (!userId) {
-                updateUserId(uuidV4());
-            }
-        });
+        if (!userId) {
+            updateUserId(uuidV4());
+        }
     };
 
     if (!isStoreSet.current) {
-        setPersistStoreValues();
         setCookiesStoresValues();
+
+        if (isClient) {
+            setPersistStoreValues();
+        }
 
         isStoreSet.current = true;
     }

shopsys/project-base

storefront/utils/store/useSetInitialStoreValues.ts
@@ -2,12 +2,14 @@ import { useRef } from 'react';
 import { CookiesStoreState } from 'store/createCookieStore';
 import { useCookiesStore } from 'store/useCookiesStore';
 import { usePersistStore } from 'store/usePersistStore';
+import { isClient } from 'utils/isClient';
 import { ServerSidePropsType } from 'utils/serverSide/initServerSideProps';
 import { v4 as uuidV4 } from 'uuid';
 
 export const useSetInitialStoreValues = ({ cookiesStore }: ServerSidePropsType) => {
     const isStoreSet = useRef(false);
 
+    const userId = usePersistStore((store) => store.userId);
     const updateUserId = usePersistStore((store) => store.updateUserId);
     const setCookieStoreValue = useCookiesStore((state) => state.setCookiesStoreState);
 
@@ -18,17 +20,17 @@ export const useSetInitialStoreValues = ({ cookiesStore }: ServerSidePropsType) 
     };
 
     const setPersistStoreValues = () => {
-        // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-        usePersistStore.persist?.onFinishHydration(({ userId }) => {
-            if (!userId) {
-                updateUserId(uuidV4());
-            }
-        });
+        if (!userId) {
+            updateUserId(uuidV4());
+        }
     };
 
     if (!isStoreSet.current) {
-        setPersistStoreValues();
         setCookiesStoresValues();
+
+        if (isClient) {
+            setPersistStoreValues();
+        }
 
         isStoreSet.current = true;
     }