shopsys/shopsys

refactored popup to use zustand instead of react portal
- now the approach we use it to modify the global state which
stores the current portal content
- because of this, we do not need to have local state with
popup data, but instead synchronously call functions
which update portal content and thus trigger rendering
- useErrorPopupVisibility was renamed to useErrorPopup
which now does not return anything but handles popup displaying
by itself

project-base/storefront/components/Pages/Order/TransportAndPayment/TransportAndPaymentSelect/PickupPlacePopup.tsx
@@ -5,31 +5,27 @@ import { TIDs } from 'cypress/tids';
 import { ListedStoreFragmentApi, TransportWithAvailablePaymentsAndStoresFragmentApi } from 'graphql/generated';
 import useTranslation from 'next-translate/useTranslation';
 import { useState } from 'react';
+import { useSessionStore } from 'store/useSessionStore';
 
 type PickupPlacePopupProps = {
     transport: TransportWithAvailablePaymentsAndStoresFragmentApi;
-    onChangePickupPlaceCallback: (selectedPickupPlace: ListedStoreFragmentApi | null) => void;
-    onClosePickupPlacePopupCallback: () => void;
+    onChangePickupPlaceCallback: (
+        transport: TransportWithAvailablePaymentsAndStoresFragmentApi,
+        selectedPickupPlace: ListedStoreFragmentApi | null,
+    ) => void;
 };
 
-export const PickupPlacePopup: FC<PickupPlacePopupProps> = ({
-    transport,
-    onChangePickupPlaceCallback,
-    onClosePickupPlacePopupCallback,
-}) => {
+export const PickupPlacePopup: FC<PickupPlacePopupProps> = ({ transport, onChangePickupPlaceCallback }) => {
     const { t } = useTranslation();
     const [selectedStoreUuid, setSelectedStoreUuid] = useState('');
+    const updatePortalContent = useSessionStore((s) => s.updatePortalContent);
 
     const onConfirmPickupPlaceHandler = () => {
         const selectedPickupPlace = transport.stores?.edges?.find(
             (storeEdge) => storeEdge?.node?.identifier === selectedStoreUuid,
         )?.node;
 
-        onChangePickupPlaceCallback(selectedPickupPlace === undefined ? null : selectedPickupPlace);
-    };
-
-    const onClosePickupPlacePopupHandler = () => {
-        onClosePickupPlacePopupCallback();
+        onChangePickupPlaceCallback(transport, selectedPickupPlace === undefined ? null : selectedPickupPlace);
     };
 
     const onSelectStoreHandler = (newStoreUuid: string | null) => {
@@ -37,11 +33,7 @@ export const PickupPlacePopup: FC<PickupPlacePopupProps> = ({
     };
 
     return (
-        <Popup
-            className="w-11/12 max-w-4xl"
-            contentClassName="overflow-y-auto"
-            onCloseCallback={onClosePickupPlacePopupHandler}
-        >
+        <Popup className="w-11/12 max-w-4xl" contentClassName="overflow-y-auto">
             <div className="h2 mb-3">{t('Choose the store where you are going to pick up your order')}</div>
             <StoreSelect
                 selectedStoreUuid={selectedStoreUuid}
@@ -49,7 +41,7 @@ export const PickupPlacePopup: FC<PickupPlacePopupProps> = ({
                 onSelectStoreCallback={onSelectStoreHandler}
             />
             <div className="mt-5 flex justify-between">
-                <Button onClick={onClosePickupPlacePopupHandler}>{t('Close')}</Button>
+                <Button onClick={() => updatePortalContent(null)}>{t('Close')}</Button>
                 <Button
                     isDisabled={selectedStoreUuid === ''}
                     tid={TIDs.pages_order_pickupplace_popup_confirm}

project-base/storefront/components/Pages/PersonalData/Overview/PersonalDataOverviewContent.tsx
@@ -11,14 +11,11 @@ import { blurInput } from 'helpers/forms/blurInput';
 import { clearForm } from 'helpers/forms/clearForm';
 import { handleFormErrors } from 'helpers/forms/handleFormErrors';
 import { showSuccessMessage } from 'helpers/toasts';
-import { useErrorPopupVisibility } from 'hooks/forms/useErrorPopupVisibility';
+import { useErrorPopup } from 'hooks/forms/useErrorPopup';
 import useTranslation from 'next-translate/useTranslation';
-import dynamic from 'next/dynamic';
 import { useCallback } from 'react';
 import { FormProvider, SubmitHandler } from 'react-hook-form';
 import { PersonalDataOverviewFormType } from 'types/form';
-
-const ErrorPopup = dynamic(() => import('components/Forms/Lib/ErrorPopup').then((component) => component.ErrorPopup));
 
 type PersonalDataOverviewContentProps = {
     contentSiteText: string | undefined;
@@ -29,7 +26,8 @@ export const PersonalDataOverviewContent: FC<PersonalDataOverviewContentProps> =
     const [, personalDataOverview] = usePersonalDataRequestMutationApi();
     const [formProviderMethods] = usePersonalDataOverviewForm();
     const formMeta = usePersonalDataOverviewFormMeta(formProviderMethods);
-    const [isErrorPopupVisible, setErrorPopupVisibility] = useErrorPopupVisibility(formProviderMethods);
+
+    useErrorPopup(formProviderMethods, formMeta.fields, undefined, GtmMessageOriginType.other);
 
     const onPersonalDataOverviewHandler = useCallback<SubmitHandler<PersonalDataOverviewFormType>>(
         async (data) => {
@@ -50,40 +48,31 @@ export const PersonalDataOverviewContent: FC<PersonalDataOverviewContentProps> =
     );
 
     return (
-        <>
-            <SimpleLayout heading={t('Personal Data Overview')}>
-                {contentSiteText !== undefined && (
-                    <div className="[&_section]:mb-5 [&_section]:block [&_section]:text-justify ">
-                        <UserText htmlContent={contentSiteText} />
-                    </div>
-                )}
-                <FormProvider {...formProviderMethods}>
-                    <Form onSubmit={formProviderMethods.handleSubmit(onPersonalDataOverviewHandler)}>
-                        <TextInputControlled
-                            control={formProviderMethods.control}
-                            formName={formMeta.formName}
-                            name={formMeta.fields.email.name}
-                            render={(textInput) => <FormLine>{textInput}</FormLine>}
-                            textInputProps={{
-                                label: formMeta.fields.email.label,
-                                required: true,
-                                type: 'email',
-                                autoComplete: 'email',
-                            }}
-                        />
-                        <div className="mt-8 flex w-full justify-center">
-                            <SubmitButton>{t('Send')}</SubmitButton>
-                        </div>
-                    </Form>
-                </FormProvider>
-            </SimpleLayout>
-            {isErrorPopupVisible && (
-                <ErrorPopup
-                    fields={formMeta.fields}
-                    gtmMessageOrigin={GtmMessageOriginType.other}
-                    onCloseCallback={() => setErrorPopupVisibility(false)}
-                />
+        <SimpleLayout heading={t('Personal Data Overview')}>
+            {contentSiteText !== undefined && (
+                <div className="[&_section]:mb-5 [&_section]:block [&_section]:text-justify ">
+                    <UserText htmlContent={contentSiteText} />
+                </div>
             )}
-        </>
+            <FormProvider {...formProviderMethods}>
+                <Form onSubmit={formProviderMethods.handleSubmit(onPersonalDataOverviewHandler)}>
+                    <TextInputControlled
+                        control={formProviderMethods.control}
+                        formName={formMeta.formName}
+                        name={formMeta.fields.email.name}
+                        render={(textInput) => <FormLine>{textInput}</FormLine>}
+                        textInputProps={{
+                            label: formMeta.fields.email.label,
+                            required: true,
+                            type: 'email',
+                            autoComplete: 'email',
+                        }}
+                    />
+                    <div className="mt-8 flex w-full justify-center">
+                        <SubmitButton>{t('Send')}</SubmitButton>
+                    </div>
+                </Form>
+            </FormProvider>
+        </SimpleLayout>
     );
 };

project-base/storefront/components/Pages/ResetPassword/ResetPasswordContent.tsx
@@ -12,21 +12,19 @@ import { clearForm } from 'helpers/forms/clearForm';
 import { handleFormErrors } from 'helpers/forms/handleFormErrors';
 import 'helpers/getInternationalizedStaticUrls';
 import { showSuccessMessage } from 'helpers/toasts';
-import { useErrorPopupVisibility } from 'hooks/forms/useErrorPopupVisibility';
+import { useErrorPopup } from 'hooks/forms/useErrorPopup';
 import useTranslation from 'next-translate/useTranslation';
-import dynamic from 'next/dynamic';
 import { useCallback } from 'react';
 import { FormProvider, SubmitHandler, useController } from 'react-hook-form';
 import { PasswordResetFormType } from 'types/form';
-
-const ErrorPopup = dynamic(() => import('components/Forms/Lib/ErrorPopup').then((component) => component.ErrorPopup));
 
 export const ResetPasswordContent: FC = () => {
     const { t } = useTranslation();
     const [, resetPassword] = usePasswordRecoveryMutationApi();
     const [formProviderMethods, defaultValues] = usePasswordResetForm();
     const formMeta = usePasswordResetFormMeta(formProviderMethods);
-    const [isErrorPopupVisible, setErrorPopupVisibility] = useErrorPopupVisibility(formProviderMethods);
+
+    useErrorPopup(formProviderMethods, formMeta.fields, undefined, GtmMessageOriginType.other);
 
     const {
         fieldState: { invalid },
@@ -50,37 +48,28 @@ export const ResetPasswordContent: FC = () => {
     );
 
     return (
-        <>
-            <SimpleLayout heading={t('Forgotten password')}>
-                <FormProvider {...formProviderMethods}>
-                    <Form onSubmit={formProviderMethods.handleSubmit(onResetPasswordHandler)}>
-                        <TextInputControlled
-                            control={formProviderMethods.control}
-                            formName={formMeta.formName}
-                            name={formMeta.fields.email.name}
-                            render={(textInput) => <FormLine>{textInput}</FormLine>}
-                            textInputProps={{
-                                label: formMeta.fields.email.label,
-                                required: true,
-                                type: 'email',
-                                autoComplete: 'email',
-                            }}
-                        />
-                        <div className="mt-8 flex w-full justify-center">
-                            <SubmitButton isWithDisabledLook={invalid || value.length === 0}>
-                                {t('Reset password')}
-                            </SubmitButton>
-                        </div>
-                    </Form>
-                </FormProvider>
-            </SimpleLayout>
-            {isErrorPopupVisible && (
-                <ErrorPopup
-                    fields={formMeta.fields}
-                    gtmMessageOrigin={GtmMessageOriginType.other}
-                    onCloseCallback={() => setErrorPopupVisibility(false)}
-                />
-            )}
-        </>
+        <SimpleLayout heading={t('Forgotten password')}>
+            <FormProvider {...formProviderMethods}>
+                <Form onSubmit={formProviderMethods.handleSubmit(onResetPasswordHandler)}>
+                    <TextInputControlled
+                        control={formProviderMethods.control}
+                        formName={formMeta.formName}
+                        name={formMeta.fields.email.name}
+                        render={(textInput) => <FormLine>{textInput}</FormLine>}
+                        textInputProps={{
+                            label: formMeta.fields.email.label,
+                            required: true,
+                            type: 'email',
+                            autoComplete: 'email',
+                        }}
+                    />
+                    <div className="mt-8 flex w-full justify-center">
+                        <SubmitButton isWithDisabledLook={invalid || value.length === 0}>
+                            {t('Reset password')}
+                        </SubmitButton>
+                    </div>
+                </Form>
+            </FormProvider>
+        </SimpleLayout>
     );
 };

project-base/storefront/hooks/forms/useErrorPopup.tsx
@@ -1,0 +1,31 @@
+import { GtmMessageOriginType } from 'gtm/types/enums';
+import dynamic from 'next/dynamic';
+import { ReactElement, useEffect } from 'react';
+import { FieldValues, UseFormReturn } from 'react-hook-form';
+import { useSessionStore } from 'store/useSessionStore';
+
+const ErrorPopup = dynamic(() => import('components/Forms/Lib/ErrorPopup').then((component) => component.ErrorPopup));
+
+export const useErrorPopup = <T extends FieldValues>(
+    formProviderMethods: UseFormReturn<T>,
+    fields: {
+        [fieldName: string]: {
+            name: string;
+            label: string | ReactElement;
+            errorMessage?: string | undefined;
+        };
+    },
+    overrideVisibility?: boolean,
+    gtmMessageOrigin?: GtmMessageOriginType,
+) => {
+    const updatePortalContent = useSessionStore((s) => s.updatePortalContent);
+
+    useEffect(() => {
+        if (
+            formProviderMethods.formState.isSubmitted &&
+            (Object.keys(formProviderMethods.formState.errors).length > 0 || overrideVisibility)
+        ) {
+            updatePortalContent(<ErrorPopup fields={fields} gtmMessageOrigin={gtmMessageOrigin} />);
+        }
+    }, [formProviderMethods.formState.isSubmitted, formProviderMethods.formState.errors, overrideVisibility]);
+};

shopsys/project-base

storefront/components/Pages/Order/TransportAndPayment/TransportAndPaymentSelect/PickupPlacePopup.tsx
@@ -6,31 +6,27 @@ import { TypeListedStoreFragment } from 'graphql/requests/stores/fragments/Liste
 import { TypeTransportWithAvailablePaymentsAndStoresFragment } from 'graphql/requests/transports/fragments/TransportWithAvailablePaymentsAndStoresFragment.generated';
 import useTranslation from 'next-translate/useTranslation';
 import { useState } from 'react';
+import { useSessionStore } from 'store/useSessionStore';
 
 type PickupPlacePopupProps = {
     transport: TypeTransportWithAvailablePaymentsAndStoresFragment;
-    onChangePickupPlaceCallback: (selectedPickupPlace: TypeListedStoreFragment | null) => void;
-    onClosePickupPlacePopupCallback: () => void;
+    onChangePickupPlaceCallback: (
+        transport: TypeTransportWithAvailablePaymentsAndStoresFragment,
+        selectedPickupPlace: TypeListedStoreFragment | null,
+    ) => void;
 };
 
-export const PickupPlacePopup: FC<PickupPlacePopupProps> = ({
-    transport,
-    onChangePickupPlaceCallback,
-    onClosePickupPlacePopupCallback,
-}) => {
+export const PickupPlacePopup: FC<PickupPlacePopupProps> = ({ transport, onChangePickupPlaceCallback }) => {
     const { t } = useTranslation();
     const [selectedStoreUuid, setSelectedStoreUuid] = useState('');
+    const updatePortalContent = useSessionStore((s) => s.updatePortalContent);
 
     const onConfirmPickupPlaceHandler = () => {
         const selectedPickupPlace = transport.stores?.edges?.find(
             (storeEdge) => storeEdge?.node?.identifier === selectedStoreUuid,
         )?.node;
 
-        onChangePickupPlaceCallback(selectedPickupPlace === undefined ? null : selectedPickupPlace);
-    };
-
-    const onClosePickupPlacePopupHandler = () => {
-        onClosePickupPlacePopupCallback();
+        onChangePickupPlaceCallback(transport, selectedPickupPlace === undefined ? null : selectedPickupPlace);
     };
 
     const onSelectStoreHandler = (newStoreUuid: string | null) => {
@@ -38,11 +34,7 @@ export const PickupPlacePopup: FC<PickupPlacePopupProps> = ({
     };
 
     return (
-        <Popup
-            className="w-11/12 max-w-4xl"
-            contentClassName="overflow-y-auto"
-            onCloseCallback={onClosePickupPlacePopupHandler}
-        >
+        <Popup className="w-11/12 max-w-4xl" contentClassName="overflow-y-auto">
             <div className="h2 mb-3">{t('Choose the store where you are going to pick up your order')}</div>
             <StoreSelect
                 selectedStoreUuid={selectedStoreUuid}
@@ -50,7 +42,7 @@ export const PickupPlacePopup: FC<PickupPlacePopupProps> = ({
                 onSelectStoreCallback={onSelectStoreHandler}
             />
             <div className="mt-5 flex justify-between">
-                <Button onClick={onClosePickupPlacePopupHandler}>{t('Close')}</Button>
+                <Button onClick={() => updatePortalContent(null)}>{t('Close')}</Button>
                 <Button
                     isDisabled={selectedStoreUuid === ''}
                     tid={TIDs.pages_order_pickupplace_popup_confirm}

storefront/components/Pages/PersonalData/Overview/PersonalDataOverviewContent.tsx
@@ -9,17 +9,14 @@ import { usePersonalDataRequestMutation } from 'graphql/requests/personalData/mu
 import { TypePersonalDataAccessRequestTypeEnum } from 'graphql/types';
 import { GtmMessageOriginType } from 'gtm/enums/GtmMessageOriginType';
 import useTranslation from 'next-translate/useTranslation';
-import dynamic from 'next/dynamic';
 import { useCallback } from 'react';
 import { FormProvider, SubmitHandler } from 'react-hook-form';
 import { PersonalDataOverviewFormType } from 'types/form';
 import { blurInput } from 'utils/forms/blurInput';
 import { clearForm } from 'utils/forms/clearForm';
 import { handleFormErrors } from 'utils/forms/handleFormErrors';
-import { useErrorPopupVisibility } from 'utils/forms/useErrorPopupVisibility';
+import { useErrorPopup } from 'utils/forms/useErrorPopup';
 import { showSuccessMessage } from 'utils/toasts/showSuccessMessage';
-
-const ErrorPopup = dynamic(() => import('components/Forms/Lib/ErrorPopup').then((component) => component.ErrorPopup));
 
 type PersonalDataOverviewContentProps = {
     contentSiteText: string | undefined;
@@ -30,7 +27,8 @@ export const PersonalDataOverviewContent: FC<PersonalDataOverviewContentProps> =
     const [, personalDataOverview] = usePersonalDataRequestMutation();
     const [formProviderMethods] = usePersonalDataOverviewForm();
     const formMeta = usePersonalDataOverviewFormMeta(formProviderMethods);
-    const [isErrorPopupVisible, setErrorPopupVisibility] = useErrorPopupVisibility(formProviderMethods);
+
+    useErrorPopup(formProviderMethods, formMeta.fields, undefined, GtmMessageOriginType.other);
 
     const onPersonalDataOverviewHandler = useCallback<SubmitHandler<PersonalDataOverviewFormType>>(
         async (data) => {
@@ -51,40 +49,31 @@ export const PersonalDataOverviewContent: FC<PersonalDataOverviewContentProps> =
     );
 
     return (
-        <>
-            <SimpleLayout heading={t('Personal Data Overview')}>
-                {contentSiteText !== undefined && (
-                    <div className="[&_section]:mb-5 [&_section]:block [&_section]:text-justify ">
-                        <UserText htmlContent={contentSiteText} />
-                    </div>
-                )}
-                <FormProvider {...formProviderMethods}>
-                    <Form onSubmit={formProviderMethods.handleSubmit(onPersonalDataOverviewHandler)}>
-                        <TextInputControlled
-                            control={formProviderMethods.control}
-                            formName={formMeta.formName}
-                            name={formMeta.fields.email.name}
-                            render={(textInput) => <FormLine>{textInput}</FormLine>}
-                            textInputProps={{
-                                label: formMeta.fields.email.label,
-                                required: true,
-                                type: 'email',
-                                autoComplete: 'email',
-                            }}
-                        />
-                        <div className="mt-8 flex w-full justify-center">
-                            <SubmitButton>{t('Send')}</SubmitButton>
-                        </div>
-                    </Form>
-                </FormProvider>
-            </SimpleLayout>
-            {isErrorPopupVisible && (
-                <ErrorPopup
-                    fields={formMeta.fields}
-                    gtmMessageOrigin={GtmMessageOriginType.other}
-                    onCloseCallback={() => setErrorPopupVisibility(false)}
-                />
+        <SimpleLayout heading={t('Personal Data Overview')}>
+            {contentSiteText !== undefined && (
+                <div className="[&_section]:mb-5 [&_section]:block [&_section]:text-justify ">
+                    <UserText htmlContent={contentSiteText} />
+                </div>
             )}
-        </>
+            <FormProvider {...formProviderMethods}>
+                <Form onSubmit={formProviderMethods.handleSubmit(onPersonalDataOverviewHandler)}>
+                    <TextInputControlled
+                        control={formProviderMethods.control}
+                        formName={formMeta.formName}
+                        name={formMeta.fields.email.name}
+                        render={(textInput) => <FormLine>{textInput}</FormLine>}
+                        textInputProps={{
+                            label: formMeta.fields.email.label,
+                            required: true,
+                            type: 'email',
+                            autoComplete: 'email',
+                        }}
+                    />
+                    <div className="mt-8 flex w-full justify-center">
+                        <SubmitButton>{t('Send')}</SubmitButton>
+                    </div>
+                </Form>
+            </FormProvider>
+        </SimpleLayout>
     );
 };

storefront/components/Pages/ResetPassword/ResetPasswordContent.tsx
@@ -9,25 +9,22 @@ import { GtmFormType } from 'gtm/enums/GtmFormType';
 import { GtmMessageOriginType } from 'gtm/enums/GtmMessageOriginType';
 import { onGtmSendFormEventHandler } from 'gtm/handlers/onGtmSendFormEventHandler';
 import useTranslation from 'next-translate/useTranslation';
-import dynamic from 'next/dynamic';
 import { useCallback } from 'react';
 import { FormProvider, SubmitHandler, useController } from 'react-hook-form';
 import { PasswordResetFormType } from 'types/form';
 import { blurInput } from 'utils/forms/blurInput';
 import { clearForm } from 'utils/forms/clearForm';
 import { handleFormErrors } from 'utils/forms/handleFormErrors';
-import { useErrorPopupVisibility } from 'utils/forms/useErrorPopupVisibility';
-import 'utils/staticUrls/getInternationalizedStaticUrls';
+import { useErrorPopup } from 'utils/forms/useErrorPopup';
 import { showSuccessMessage } from 'utils/toasts/showSuccessMessage';
-
-const ErrorPopup = dynamic(() => import('components/Forms/Lib/ErrorPopup').then((component) => component.ErrorPopup));
 
 export const ResetPasswordContent: FC = () => {
     const { t } = useTranslation();
     const [, resetPassword] = usePasswordRecoveryMutation();
     const [formProviderMethods, defaultValues] = usePasswordResetForm();
     const formMeta = usePasswordResetFormMeta(formProviderMethods);
-    const [isErrorPopupVisible, setErrorPopupVisibility] = useErrorPopupVisibility(formProviderMethods);
+
+    useErrorPopup(formProviderMethods, formMeta.fields, undefined, GtmMessageOriginType.other);
 
     const {
         fieldState: { invalid },
@@ -51,37 +48,28 @@ export const ResetPasswordContent: FC = () => {
     );
 
     return (
-        <>
-            <SimpleLayout heading={t('Forgotten password')}>
-                <FormProvider {...formProviderMethods}>
-                    <Form onSubmit={formProviderMethods.handleSubmit(onResetPasswordHandler)}>
-                        <TextInputControlled
-                            control={formProviderMethods.control}
-                            formName={formMeta.formName}
-                            name={formMeta.fields.email.name}
-                            render={(textInput) => <FormLine>{textInput}</FormLine>}
-                            textInputProps={{
-                                label: formMeta.fields.email.label,
-                                required: true,
-                                type: 'email',
-                                autoComplete: 'email',
-                            }}
-                        />
-                        <div className="mt-8 flex w-full justify-center">
-                            <SubmitButton isWithDisabledLook={invalid || value.length === 0}>
-                                {t('Reset password')}
-                            </SubmitButton>
-                        </div>
-                    </Form>
-                </FormProvider>
-            </SimpleLayout>
-            {isErrorPopupVisible && (
-                <ErrorPopup
-                    fields={formMeta.fields}
-                    gtmMessageOrigin={GtmMessageOriginType.other}
-                    onCloseCallback={() => setErrorPopupVisibility(false)}
-                />
-            )}
-        </>
+        <SimpleLayout heading={t('Forgotten password')}>
+            <FormProvider {...formProviderMethods}>
+                <Form onSubmit={formProviderMethods.handleSubmit(onResetPasswordHandler)}>
+                    <TextInputControlled
+                        control={formProviderMethods.control}
+                        formName={formMeta.formName}
+                        name={formMeta.fields.email.name}
+                        render={(textInput) => <FormLine>{textInput}</FormLine>}
+                        textInputProps={{
+                            label: formMeta.fields.email.label,
+                            required: true,
+                            type: 'email',
+                            autoComplete: 'email',
+                        }}
+                    />
+                    <div className="mt-8 flex w-full justify-center">
+                        <SubmitButton isWithDisabledLook={invalid || value.length === 0}>
+                            {t('Reset password')}
+                        </SubmitButton>
+                    </div>
+                </Form>
+            </FormProvider>
+        </SimpleLayout>
     );
 };

storefront/utils/forms/useErrorPopup.tsx
@@ -1,0 +1,31 @@
+import { GtmMessageOriginType } from 'gtm/enums/GtmMessageOriginType';
+import dynamic from 'next/dynamic';
+import { ReactElement, useEffect } from 'react';
+import { FieldValues, UseFormReturn } from 'react-hook-form';
+import { useSessionStore } from 'store/useSessionStore';
+
+const ErrorPopup = dynamic(() => import('components/Forms/Lib/ErrorPopup').then((component) => component.ErrorPopup));
+
+export const useErrorPopup = <T extends FieldValues>(
+    formProviderMethods: UseFormReturn<T>,
+    fields: {
+        [fieldName: string]: {
+            name: string;
+            label: string | ReactElement;
+            errorMessage?: string | undefined;
+        };
+    },
+    overrideVisibility?: boolean,
+    gtmMessageOrigin?: GtmMessageOriginType,
+) => {
+    const updatePortalContent = useSessionStore((s) => s.updatePortalContent);
+
+    useEffect(() => {
+        if (
+            formProviderMethods.formState.isSubmitted &&
+            (Object.keys(formProviderMethods.formState.errors).length > 0 || overrideVisibility)
+        ) {
+            updatePortalContent(<ErrorPopup fields={fields} gtmMessageOrigin={gtmMessageOrigin} />);
+        }
+    }, [formProviderMethods.formState.isSubmitted, formProviderMethods.formState.errors, overrideVisibility]);
+};