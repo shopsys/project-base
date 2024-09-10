shopsys/shopsys

removed deprecated dash between docker and compose commands

Makefile
@@ -1,11 +1,11 @@
 generate-schema:
-	docker-compose exec php-fpm php phing frontend-api-generate-graphql-schema
+	docker compose exec php-fpm php phing frontend-api-generate-graphql-schema
 	docker cp shopsys-framework-php-fpm:/var/www/html/project-base/app/schema.graphql /tmp/schema.graphql
 	docker cp /tmp/schema.graphql shopsys-framework-storefront:/home/node/app/schema.graphql
-	docker-compose exec -u root storefront chown node:node schema.graphql
+	docker compose exec -u root storefront chown node:node schema.graphql
 	find project-base/storefront/graphql/requests -type f -name "*.generated.tsx" -exec rm {} \;
-	docker-compose exec storefront pnpm run gql
-	docker-compose exec storefront rm -rf /home/node/app/schema.graphql
+	docker compose exec storefront pnpm run gql
+	docker compose exec storefront rm -rf /home/node/app/schema.graphql
 
 generate-schema-native:
 	php phing frontend-api-generate-graphql-schema
@@ -15,11 +15,11 @@ generate-schema-native:
 	rm -rf project-base/storefront/schema.graphql
 
 check-schema:
-	docker-compose exec php-fpm php phing frontend-api-generate-graphql-schema
+	docker compose exec php-fpm php phing frontend-api-generate-graphql-schema
 	docker cp shopsys-framework-php-fpm:/var/www/html/project-base/app/schema.graphql /tmp/schema.graphql
 	docker cp /tmp/schema.graphql shopsys-framework-storefront:/home/node/app/schema.graphql
-	docker-compose exec -u root storefront chown node:node schema.graphql
-	docker-compose exec storefront sh check-code-gen.sh
+	docker compose exec -u root storefront chown node:node schema.graphql
+	docker compose exec storefront sh check-code-gen.sh
 
 define prepare-data-for-acceptance-tests
 	docker compose exec php-fpm php phing -D production.confirm.action=y -D change.environment=test environment-change

shopsys/project-base

Makefile
@@ -1,10 +1,10 @@
 generate-schema:
-	docker-compose exec php-fpm php phing frontend-api-generate-graphql-schema
+	docker compose exec php-fpm php phing frontend-api-generate-graphql-schema
 	docker cp shopsys-framework-php-fpm:/var/www/html/schema.graphql /tmp/schema.graphql
 	docker cp /tmp/schema.graphql shopsys-framework-storefront:/home/node/app/schema.graphql
-	docker-compose exec -u root storefront chown node:node schema.graphql
-	docker-compose exec storefront npm run gql
-	docker-compose exec storefront rm -rf /home/node/app/schema.graphql
+	docker compose exec -u root storefront chown node:node schema.graphql
+	docker compose exec storefront npm run gql
+	docker compose exec storefront rm -rf /home/node/app/schema.graphql
 
 generate-schema-native:
 	cd app; php phing frontend-api-generate-graphql-schema