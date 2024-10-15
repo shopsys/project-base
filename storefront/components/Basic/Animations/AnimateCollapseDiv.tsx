mdsxbm/shopsys

add more animations and unify current animations  - new animations for OrderSummary transport & payment add/remove  - new animations for TransportAndPayment selects  - unify cart and user menu open animations   - new `Drawer` component was extracted to be used for menus on mobile devices   - desktop menus on hover were moved to components called `*Popover`   - new `Animated*Div` components were added with predefined animations  - new animation for `ReactSelect` menu open  - unify animation for `Popup`  - minor refactor of `SalesRepresentative`, `CartInHeader` and `MenuIconicItemUserAuthenticated` components

project-base/storefront/components/Basic/Animations/AnimateCollapseDiv.tsx
@@ -1,0 +1,24 @@
+import { TIDs } from 'cypress/tids';
+import { HTMLMotionProps, m } from 'framer-motion';
+import { collapseExpandAnimation } from 'utils/animations/animationVariants';
+
+export const AnimateCollapseDiv: FC<HTMLMotionProps<'div'> & { tid?: TIDs; key?: string }> = ({
+    children,
+    className,
+    key,
+    tid,
+    ...props
+}) => (
+    <m.div
+        key={key}
+        animate="open"
+        className={className}
+        exit="closed"
+        initial="closed"
+        tid={tid}
+        variants={collapseExpandAnimation}
+        {...props}
+    >
+        {children}
+    </m.div>
+);

project-base/storefront/components/Basic/Loader/LoaderWithOverlay.tsx
@@ -3,7 +3,7 @@ import { TIDs } from 'cypress/tids';
 
 export const LoaderWithOverlay: FC = ({ className }) => (
     <div
-        className="absolute inset-0 z-overlay flex h-full w-full items-center justify-center bg-overlay"
+        className="absolute inset-0 z-overlay flex h-full w-full items-center justify-center bg-imageOverlay rounded-sm backdrop-blur-sm"
         tid={TIDs.loader_overlay}
     >
         <SpinnerIcon className={className} />

shopsys/project-base

storefront/components/Basic/Animations/AnimateCollapseDiv.tsx
@@ -1,0 +1,24 @@
+import { TIDs } from 'cypress/tids';
+import { HTMLMotionProps, m } from 'framer-motion';
+import { collapseExpandAnimation } from 'utils/animations/animationVariants';
+
+export const AnimateCollapseDiv: FC<HTMLMotionProps<'div'> & { tid?: TIDs; keyName?: string }> = ({
+    children,
+    className,
+    keyName,
+    tid,
+    ...props
+}) => (
+    <m.div
+        key={keyName}
+        animate="open"
+        className={className}
+        exit="closed"
+        initial="closed"
+        tid={tid}
+        variants={collapseExpandAnimation}
+        {...props}
+    >
+        {children}
+    </m.div>
+);

storefront/components/Basic/Loader/LoaderWithOverlay.tsx
@@ -3,7 +3,7 @@ import { TIDs } from 'cypress/tids';
 
 export const LoaderWithOverlay: FC = ({ className }) => (
     <div
-        className="absolute inset-0 z-overlay flex h-full w-full items-center justify-center bg-overlay"
+        className="absolute inset-0 z-overlay flex h-full w-full items-center justify-center rounded-sm bg-imageOverlay backdrop-blur-sm"
         tid={TIDs.loader_overlay}
     >
         <SpinnerIcon className={className} />