7425c3c68914f96ea206e028a88d1a1420607045
Michal Vanek <michal.vanek@shopsys.com>
2024-10-31 16:49:07 +0100

    Extend banner administration for description box
    - create NumberSlider form widget with jQuery UI
    - extend SliderItem, SliderItemData, SliderItemDataFixture for description, rgbBackgroundColor, opacity

--- app/src/DataFixtures/Demo/SliderItemDataFixture.php
+++ app/src/DataFixtures/Demo/SliderItemDataFixture.php
@@ -45,16 +45,25 @@ class SliderItemDataFixture extends AbstractReferenceFixture
 
             $sliderItemData->name = 'Shopsys';
             $sliderItemData->link = 'https://www.shopsys.cz';
+            $sliderItemData->description = t('This slider item promotes our latest offers and updates. Stay tuned for more exciting news and promotions. Click the link to learn more!', [], Translator::DATA_FIXTURES_TRANSLATION_DOMAIN, $locale);
+            $sliderItemData->rgbBackgroundColor = '#4844bd';
+            $sliderItemData->opacity = 0.5;
 
             $this->sliderItemFacade->create($sliderItemData);
 
             $sliderItemData->name = t('Documentation', [], Translator::DATA_FIXTURES_TRANSLATION_DOMAIN, $locale);
+            $sliderItemData->description = t('Explore our comprehensive documentation to get the most out of our platform. Find guides, tutorials, and detailed information to help you navigate and utilize all features effectively. Click the link to access the full documentation.', [], Translator::DATA_FIXTURES_TRANSLATION_DOMAIN, $locale);
+            $sliderItemData->rgbBackgroundColor = '#808080';
+            $sliderItemData->opacity = 0.5;
             $sliderItemData->link = 'https://docs.shopsys.com';
             $sliderItemData->uuid = Uuid::uuid5(self::UUID_NAMESPACE, 'Documentation' . $domainId)->toString();
 
             $this->sliderItemFacade->create($sliderItemData);
 
             $sliderItemData->name = t('Become one of us', [], Translator::DATA_FIXTURES_TRANSLATION_DOMAIN, $locale);
+            $sliderItemData->description = t('Join our team and be part of an innovative company. Explore exciting career opportunities and grow with us!', [], Translator::DATA_FIXTURES_TRANSLATION_DOMAIN, $locale);
+            $sliderItemData->rgbBackgroundColor = '#b3bf45';
+            $sliderItemData->opacity = 0.5;
             $sliderItemData->link = 'https://jobs.shopsys.cz';
             $sliderItemData->uuid = Uuid::uuid5(self::UUID_NAMESPACE, 'Become one of us' . $domainId)->toString();
 
