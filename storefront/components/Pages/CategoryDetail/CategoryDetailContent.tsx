shopsys/shopsys

added brand and flag page filtration

project-base/storefront/components/Pages/CategoryDetail/CategoryDetailContent.tsx
@@ -1,6 +1,6 @@
 import { DeferredCategoryDetailProductsWrapper } from './CategoryDetailProductsWrapper/DeferredCategoryDetailProductsWrapper';
-import { CollapsibleText } from 'components/Basic/CollapsibleText/CollapsibleText';
-import { Image } from 'components/Basic/Image/Image';
+import { CollapsibleDescriptionWithImage } from 'components/Blocks/CollapsibleDescriptionWithImage/CollapsibleDescriptionWithImage';
+import { FilteredProductsWrapper } from 'components/Blocks/FilteredProductsWrapper/FilteredProductsWrapper';
 import { DeferredFilterPanel } from 'components/Blocks/Product/Filter/DeferredFilterPanel';
 import { FilterSelectedParameters } from 'components/Blocks/Product/Filter/FilterSelectedParameters';
 import { SimpleNavigation } from 'components/Blocks/SimpleNavigation/SimpleNavigation';
@@ -10,14 +10,9 @@ import { TypeCategoryDetailFragment } from 'graphql/requests/categories/fragment
 import { useGtmFriendlyPageViewEvent } from 'gtm/factories/useGtmFriendlyPageViewEvent';
 import { useGtmPageViewEvent } from 'gtm/utils/pageViewEvents/useGtmPageViewEvent';
 import dynamic from 'next/dynamic';
-import { useRef, useState } from 'react';
-import { twJoin } from 'tailwind-merge';
+import { useRef } from 'react';
 import { useCurrentPageQuery } from 'utils/queryParams/useCurrentPageQuery';
 import { useSeoTitleWithPagination } from 'utils/seo/useSeoTitleWithPagination';
-
-const Overlay = dynamic(() => import('components/Basic/Overlay/Overlay').then((component) => component.Overlay), {
-    ssr: false,
-});
 
 const AdvancedSeoCategories = dynamic(() =>
     import('./AdvancedSeoCategories').then((component) => component.AdvancedSeoCategories),
@@ -33,23 +28,11 @@ type CategoryDetailContentProps = {
 };
 
 export const CategoryDetailContent: FC<CategoryDetailContentProps> = ({ category, isFetchingVisible }) => {
-    const [isPanelOpen, setIsPanelOpen] = useState(false);
+    const scrollTargetRef = useRef<HTMLDivElement>(null);
     const paginationScrollTargetRef = useRef<HTMLDivElement>(null);
     const currentPage = useCurrentPageQuery();
-    const scrollTargetRef = useRef<HTMLDivElement>(null);
 
     const title = useSeoTitleWithPagination(category.products.totalCount, category.name, category.seoH1);
-
-    const handlePanelOpenerClick = () => {
-        const body = document.getElementsByTagName('body')[0];
-
-        setIsPanelOpen((prev) => {
-            const newValue = !prev;
-            body.style.overflow = newValue ? 'hidden' : 'visible';
-
-            return newValue;
-        });
-    };
 
     const pageViewEvent = useGtmFriendlyPageViewEvent(category);
     useGtmPageViewEvent(pageViewEvent, isFetchingVisible);
@@ -57,20 +40,14 @@ export const CategoryDetailContent: FC<CategoryDetailContentProps> = ({ category
     return (
         <Webline>
             <h1 ref={scrollTargetRef}>{title}</h1>
-            <div className="mb-7 flex w-full flex-col-reverse justify-between gap-5 vl:flex-row">
-                {!!category.description && currentPage === 1 && (
-                    <CollapsibleText scrollTargetRef={scrollTargetRef} text={category.description} />
-                )}
-                <div className="h-full flex-shrink-0 sm:h-32">
-                    <Image
-                        alt={category.name}
-                        className="h-full w-auto rounded-lg"
-                        height={500}
-                        src={category.images[0]?.url}
-                        width={500}
-                    />
-                </div>
-            </div>
+
+            <CollapsibleDescriptionWithImage
+                currentPage={currentPage}
+                description={category.description}
+                imageName={category.images[0].name || category.name}
+                imageUrl={category.images[0].url}
+                scrollTargetRef={scrollTargetRef}
+            />
 
             <SimpleNavigation
                 isWithoutSlider
@@ -79,28 +56,15 @@ export const CategoryDetailContent: FC<CategoryDetailContentProps> = ({ category
                 listedItems={[...category.children, ...category.linkedCategories]}
             />
 
-            <div
-                className="mb-8 flex scroll-mt-5 flex-col vl:mb-10 vl:flex-row vl:flex-wrap vl:gap-4"
-                ref={paginationScrollTargetRef}
-            >
-                <div
-                    className={twJoin(
-                        'fixed bottom-0 left-0 right-10 top-0 max-w-[400px] -translate-x-full overflow-hidden transition max-vl:z-aboveOverlay vl:static vl:w-[227px] vl:translate-x-0 vl:rounded-none vl:transition-none',
-                        isPanelOpen && 'translate-x-0',
-                    )}
-                >
-                    <DeferredFilterPanel
-                        defaultOrderingMode={category.products.defaultOrderingMode}
-                        orderingMode={category.products.orderingMode}
-                        originalSlug={category.originalCategorySlug}
-                        panelCloseHandler={handlePanelOpenerClick}
-                        productFilterOptions={category.products.productFilterOptions}
-                        slug={category.slug}
-                        totalCount={category.products.totalCount}
-                    />
-                </div>
-
-                {isPanelOpen && <Overlay isActive={isPanelOpen} onClick={handlePanelOpenerClick} />}
+            <FilteredProductsWrapper ref={paginationScrollTargetRef}>
+                <DeferredFilterPanel
+                    defaultOrderingMode={category.products.defaultOrderingMode}
+                    orderingMode={category.products.orderingMode}
+                    originalSlug={category.originalCategorySlug}
+                    productFilterOptions={category.products.productFilterOptions}
+                    slug={category.slug}
+                    totalCount={category.products.totalCount}
+                />
 
                 <div className="flex flex-1 flex-col">
                     {!!category.bestsellers.length && <CategoryBestsellers products={category.bestsellers} />}
@@ -109,7 +73,6 @@ export const CategoryDetailContent: FC<CategoryDetailContentProps> = ({ category
                         <FilterSelectedParameters filterOptions={category.products.productFilterOptions} />
 
                         <DeferredFilterAndSortingBar
-                            handlePanelOpenerClick={handlePanelOpenerClick}
                             sorting={category.products.orderingMode}
                             totalCount={category.products.totalCount}
                         />
@@ -120,7 +83,8 @@ export const CategoryDetailContent: FC<CategoryDetailContentProps> = ({ category
                         paginationScrollTargetRef={paginationScrollTargetRef}
                     />
                 </div>
-            </div>
+            </FilteredProductsWrapper>
+
             {!!category.readyCategorySeoMixLinks.length && (
                 <AdvancedSeoCategories readyCategorySeoMixLinks={category.readyCategorySeoMixLinks} />
             )}

project-base/storefront/components/Pages/FlagDetail/FlagDetailContent.tsx
@@ -1,4 +1,7 @@
 import { FlagDetailProductsWrapper } from './FlagDetailProductsWrapper';
+import { FilteredProductsWrapper } from 'components/Blocks/FilteredProductsWrapper/FilteredProductsWrapper';
+import { DeferredFilterPanel } from 'components/Blocks/Product/Filter/DeferredFilterPanel';
+import { FilterSelectedParameters } from 'components/Blocks/Product/Filter/FilterSelectedParameters';
 import { DeferredFilterAndSortingBar } from 'components/Blocks/SortingBar/DeferredFilterAndSortingBar';
 import { Webline } from 'components/Layout/Webline/Webline';
 import { TypeFlagDetailFragment } from 'graphql/requests/flags/fragments/FlagDetailFragment.generated';
@@ -12,19 +15,32 @@ export const FlagDetailContent: FC<FlagDetailContentProps> = ({ flag }) => {
     const paginationScrollTargetRef = useRef<HTMLDivElement>(null);
 
     return (
-        <>
-            <Webline>
-                <h1>{flag.name}</h1>
-            </Webline>
-            <Webline>
-                <div className="scroll-mt-5" ref={paginationScrollTargetRef}>
-                    <DeferredFilterAndSortingBar
-                        sorting={flag.products.orderingMode}
-                        totalCount={flag.products.totalCount}
-                    />
+        <Webline>
+            <h1>{flag.name}</h1>
+
+            <FilteredProductsWrapper ref={paginationScrollTargetRef}>
+                <DeferredFilterPanel
+                    defaultOrderingMode={flag.products.defaultOrderingMode}
+                    orderingMode={flag.products.orderingMode}
+                    originalSlug={flag.slug}
+                    productFilterOptions={flag.products.productFilterOptions}
+                    slug={flag.slug}
+                    totalCount={flag.products.totalCount}
+                />
+
+                <div className="flex flex-1 flex-col">
+                    <div className="flex flex-col">
+                        <FilterSelectedParameters filterOptions={flag.products.productFilterOptions} />
+
+                        <DeferredFilterAndSortingBar
+                            sorting={flag.products.orderingMode}
+                            totalCount={flag.products.totalCount}
+                        />
+                    </div>
+
                     <FlagDetailProductsWrapper flag={flag} paginationScrollTargetRef={paginationScrollTargetRef} />
                 </div>
-            </Webline>
-        </>
+            </FilteredProductsWrapper>
+        </Webline>
     );
 };

project-base/storefront/components/Pages/Search/SearchProducts.tsx
@@ -1,39 +1,24 @@
 import { SearchProductsContent } from './SearchProductsContent';
 import { useSearchProductsData } from './searchUtils';
-import { FilterPanel } from 'components/Blocks/Product/Filter/FilterPanel';
+import { FilteredProductsWrapper } from 'components/Blocks/FilteredProductsWrapper/FilteredProductsWrapper';
+import { DeferredFilterPanel } from 'components/Blocks/Product/Filter/DeferredFilterPanel';
 import { FilterSelectedParameters } from 'components/Blocks/Product/Filter/FilterSelectedParameters';
 import { SkeletonModuleProductsList } from 'components/Blocks/Skeleton/SkeletonModuleProductsList';
 import { DeferredFilterAndSortingBar } from 'components/Blocks/SortingBar/DeferredFilterAndSortingBar';
 import { useDomainConfig } from 'components/providers/DomainConfigProvider';
 import { TypeProductOrderingModeEnum } from 'graphql/types';
 import useTranslation from 'next-translate/useTranslation';
-import dynamic from 'next/dynamic';
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import Skeleton from 'react-loading-skeleton';
-import { twJoin } from 'tailwind-merge';
 import { getInternationalizedStaticUrls } from 'utils/staticUrls/getInternationalizedStaticUrls';
-
-const Overlay = dynamic(() => import('components/Basic/Overlay/Overlay').then((component) => component.Overlay));
 
 export const SearchProducts: FC = () => {
     const { t } = useTranslation();
     const paginationScrollTargetRef = useRef<HTMLDivElement>(null);
     const { url } = useDomainConfig();
     const [searchUrl] = getInternationalizedStaticUrls(['/search'], url);
-    const [isPanelOpen, setIsPanelOpen] = useState(false);
 
     const { searchProductsData, areSearchProductsFetching, isLoadingMoreSearchProducts } = useSearchProductsData();
-
-    const handlePanelOpenerClick = () => {
-        const body = document.getElementsByTagName('body')[0];
-
-        setIsPanelOpen((prev) => {
-            const newValue = !prev;
-            body.style.overflow = newValue ? 'hidden' : 'visible';
-
-            return newValue;
-        });
-    };
 
     if (areSearchProductsFetching) {
         return (
@@ -52,32 +37,21 @@ export const SearchProducts: FC = () => {
         <>
             <h5 className="mb-2 mt-5 lg:my-9">{t('Found products')}</h5>
 
-            <div className="mb-8 flex scroll-mt-5 flex-col vl:mb-10 vl:flex-row vl:flex-wrap vl:gap-4">
-                <div
-                    className={twJoin(
-                        'fixed bottom-0 left-0 right-10 top-0 max-w-[400px] -translate-x-full overflow-hidden transition max-vl:z-aboveOverlay vl:static vl:w-[227px] vl:translate-x-0 vl:rounded-none vl:transition-none',
-                        isPanelOpen && 'translate-x-0',
-                    )}
-                >
-                    <FilterPanel
-                        defaultOrderingMode={searchProductsData.defaultOrderingMode}
-                        orderingMode={searchProductsData.orderingMode}
-                        originalSlug={null}
-                        panelCloseHandler={handlePanelOpenerClick}
-                        productFilterOptions={searchProductsData.productFilterOptions}
-                        slug={searchUrl}
-                        totalCount={searchProductsData.totalCount}
-                    />
-                </div>
+            <FilteredProductsWrapper ref={paginationScrollTargetRef}>
+                <DeferredFilterPanel
+                    defaultOrderingMode={searchProductsData.defaultOrderingMode}
+                    orderingMode={searchProductsData.orderingMode}
+                    originalSlug={null}
+                    productFilterOptions={searchProductsData.productFilterOptions}
+                    slug={searchUrl}
+                    totalCount={searchProductsData.totalCount}
+                />
 
-                <Overlay isActive={isPanelOpen} onClick={handlePanelOpenerClick} />
-
-                <div className="flex flex-1 flex-col" ref={paginationScrollTargetRef}>
+                <div className="flex flex-1 flex-col">
                     <div className="flex flex-col">
                         <FilterSelectedParameters filterOptions={searchProductsData.productFilterOptions} />
 
                         <DeferredFilterAndSortingBar
-                            handlePanelOpenerClick={handlePanelOpenerClick}
                             sorting={searchProductsData.orderingMode}
                             totalCount={searchProductsData.totalCount}
                             customSortOptions={[
@@ -95,7 +69,7 @@ export const SearchProducts: FC = () => {
                         searchProductsData={searchProductsData}
                     />
                 </div>
-            </div>
+            </FilteredProductsWrapper>
         </>
     );
 };

shopsys/project-base

storefront/components/Pages/CategoryDetail/CategoryDetailContent.tsx
@@ -1,6 +1,6 @@
 import { DeferredCategoryDetailProductsWrapper } from './CategoryDetailProductsWrapper/DeferredCategoryDetailProductsWrapper';
-import { CollapsibleText } from 'components/Basic/CollapsibleText/CollapsibleText';
-import { Image } from 'components/Basic/Image/Image';
+import { CollapsibleDescriptionWithImage } from 'components/Blocks/CollapsibleDescriptionWithImage/CollapsibleDescriptionWithImage';
+import { FilteredProductsWrapper } from 'components/Blocks/FilteredProductsWrapper/FilteredProductsWrapper';
 import { DeferredFilterPanel } from 'components/Blocks/Product/Filter/DeferredFilterPanel';
 import { FilterSelectedParameters } from 'components/Blocks/Product/Filter/FilterSelectedParameters';
 import { SimpleNavigation } from 'components/Blocks/SimpleNavigation/SimpleNavigation';
@@ -10,14 +10,9 @@ import { TypeCategoryDetailFragment } from 'graphql/requests/categories/fragment
 import { useGtmFriendlyPageViewEvent } from 'gtm/factories/useGtmFriendlyPageViewEvent';
 import { useGtmPageViewEvent } from 'gtm/utils/pageViewEvents/useGtmPageViewEvent';
 import dynamic from 'next/dynamic';
-import { useRef, useState } from 'react';
-import { twJoin } from 'tailwind-merge';
+import { useRef } from 'react';
 import { useCurrentPageQuery } from 'utils/queryParams/useCurrentPageQuery';
 import { useSeoTitleWithPagination } from 'utils/seo/useSeoTitleWithPagination';
-
-const Overlay = dynamic(() => import('components/Basic/Overlay/Overlay').then((component) => component.Overlay), {
-    ssr: false,
-});
 
 const AdvancedSeoCategories = dynamic(() =>
     import('./AdvancedSeoCategories').then((component) => component.AdvancedSeoCategories),
@@ -33,23 +28,11 @@ type CategoryDetailContentProps = {
 };
 
 export const CategoryDetailContent: FC<CategoryDetailContentProps> = ({ category, isFetchingVisible }) => {
-    const [isPanelOpen, setIsPanelOpen] = useState(false);
+    const scrollTargetRef = useRef<HTMLDivElement>(null);
     const paginationScrollTargetRef = useRef<HTMLDivElement>(null);
     const currentPage = useCurrentPageQuery();
-    const scrollTargetRef = useRef<HTMLDivElement>(null);
 
     const title = useSeoTitleWithPagination(category.products.totalCount, category.name, category.seoH1);
-
-    const handlePanelOpenerClick = () => {
-        const body = document.getElementsByTagName('body')[0];
-
-        setIsPanelOpen((prev) => {
-            const newValue = !prev;
-            body.style.overflow = newValue ? 'hidden' : 'visible';
-
-            return newValue;
-        });
-    };
 
     const pageViewEvent = useGtmFriendlyPageViewEvent(category);
     useGtmPageViewEvent(pageViewEvent, isFetchingVisible);
@@ -57,22 +40,14 @@ export const CategoryDetailContent: FC<CategoryDetailContentProps> = ({ category
     return (
         <Webline>
             <h1 ref={scrollTargetRef}>{title}</h1>
-            <div className="mb-7 flex w-full flex-col-reverse justify-between gap-5 vl:flex-row">
-                {!!category.description && currentPage === 1 && (
-                    <CollapsibleText scrollTargetRef={scrollTargetRef} text={category.description} />
-                )}
-                {currentPage === 1 && (
-                    <div className="h-full flex-shrink-0 sm:h-32">
-                        <Image
-                            alt={category.name}
-                            className="h-full w-auto rounded-lg"
-                            height={500}
-                            src={category.images[0]?.url}
-                            width={500}
-                        />
-                    </div>
-                )}
-            </div>
+
+            <CollapsibleDescriptionWithImage
+                currentPage={currentPage}
+                description={category.description}
+                imageName={category.images[0].name || category.name}
+                imageUrl={category.images[0].url}
+                scrollTargetRef={scrollTargetRef}
+            />
 
             <SimpleNavigation
                 isWithoutSlider
@@ -81,28 +56,15 @@ export const CategoryDetailContent: FC<CategoryDetailContentProps> = ({ category
                 listedItems={[...category.children, ...category.linkedCategories]}
             />
 
-            <div
-                className="mb-8 flex scroll-mt-5 flex-col vl:mb-10 vl:flex-row vl:flex-wrap vl:gap-4"
-                ref={paginationScrollTargetRef}
-            >
-                <div
-                    className={twJoin(
-                        'fixed bottom-0 left-0 right-10 top-0 max-w-[400px] -translate-x-full overflow-hidden transition max-vl:z-aboveOverlay vl:static vl:w-[227px] vl:translate-x-0 vl:rounded-none vl:transition-none',
-                        isPanelOpen && 'translate-x-0',
-                    )}
-                >
-                    <DeferredFilterPanel
-                        defaultOrderingMode={category.products.defaultOrderingMode}
-                        orderingMode={category.products.orderingMode}
-                        originalSlug={category.originalCategorySlug}
-                        panelCloseHandler={handlePanelOpenerClick}
-                        productFilterOptions={category.products.productFilterOptions}
-                        slug={category.slug}
-                        totalCount={category.products.totalCount}
-                    />
-                </div>
-
-                {isPanelOpen && <Overlay isActive={isPanelOpen} onClick={handlePanelOpenerClick} />}
+            <FilteredProductsWrapper paginationScrollTargetRef={paginationScrollTargetRef}>
+                <DeferredFilterPanel
+                    defaultOrderingMode={category.products.defaultOrderingMode}
+                    orderingMode={category.products.orderingMode}
+                    originalSlug={category.originalCategorySlug}
+                    productFilterOptions={category.products.productFilterOptions}
+                    slug={category.slug}
+                    totalCount={category.products.totalCount}
+                />
 
                 <div className="flex flex-1 flex-col">
                     {!!category.bestsellers.length && <CategoryBestsellers products={category.bestsellers} />}
@@ -111,7 +73,6 @@ export const CategoryDetailContent: FC<CategoryDetailContentProps> = ({ category
                         <FilterSelectedParameters filterOptions={category.products.productFilterOptions} />
 
                         <DeferredFilterAndSortingBar
-                            handlePanelOpenerClick={handlePanelOpenerClick}
                             sorting={category.products.orderingMode}
                             totalCount={category.products.totalCount}
                         />
@@ -122,7 +83,8 @@ export const CategoryDetailContent: FC<CategoryDetailContentProps> = ({ category
                         paginationScrollTargetRef={paginationScrollTargetRef}
                     />
                 </div>
-            </div>
+            </FilteredProductsWrapper>
+
             {!!category.readyCategorySeoMixLinks.length && (
                 <AdvancedSeoCategories readyCategorySeoMixLinks={category.readyCategorySeoMixLinks} />
             )}

storefront/components/Pages/FlagDetail/FlagDetailContent.tsx
@@ -1,4 +1,7 @@
 import { FlagDetailProductsWrapper } from './FlagDetailProductsWrapper';
+import { FilteredProductsWrapper } from 'components/Blocks/FilteredProductsWrapper/FilteredProductsWrapper';
+import { DeferredFilterPanel } from 'components/Blocks/Product/Filter/DeferredFilterPanel';
+import { FilterSelectedParameters } from 'components/Blocks/Product/Filter/FilterSelectedParameters';
 import { DeferredFilterAndSortingBar } from 'components/Blocks/SortingBar/DeferredFilterAndSortingBar';
 import { Webline } from 'components/Layout/Webline/Webline';
 import { TypeFlagDetailFragment } from 'graphql/requests/flags/fragments/FlagDetailFragment.generated';
@@ -11,20 +14,35 @@ type FlagDetailContentProps = {
 export const FlagDetailContent: FC<FlagDetailContentProps> = ({ flag }) => {
     const paginationScrollTargetRef = useRef<HTMLDivElement>(null);
 
+    flag.products.productFilterOptions.flags = null;
+
     return (
-        <>
-            <Webline>
-                <h1>{flag.name}</h1>
-            </Webline>
-            <Webline>
-                <div className="scroll-mt-5" ref={paginationScrollTargetRef}>
-                    <DeferredFilterAndSortingBar
-                        sorting={flag.products.orderingMode}
-                        totalCount={flag.products.totalCount}
-                    />
+        <Webline>
+            <h1>{flag.name}</h1>
+
+            <FilteredProductsWrapper paginationScrollTargetRef={paginationScrollTargetRef}>
+                <DeferredFilterPanel
+                    defaultOrderingMode={flag.products.defaultOrderingMode}
+                    orderingMode={flag.products.orderingMode}
+                    originalSlug={flag.slug}
+                    productFilterOptions={flag.products.productFilterOptions}
+                    slug={flag.slug}
+                    totalCount={flag.products.totalCount}
+                />
+
+                <div className="flex flex-1 flex-col">
+                    <div className="flex flex-col-reverse vl:flex-col">
+                        <FilterSelectedParameters filterOptions={flag.products.productFilterOptions} />
+
+                        <DeferredFilterAndSortingBar
+                            sorting={flag.products.orderingMode}
+                            totalCount={flag.products.totalCount}
+                        />
+                    </div>
+
                     <FlagDetailProductsWrapper flag={flag} paginationScrollTargetRef={paginationScrollTargetRef} />
                 </div>
-            </Webline>
-        </>
+            </FilteredProductsWrapper>
+        </Webline>
     );
 };

storefront/components/Pages/Search/SearchProducts.tsx
@@ -1,37 +1,22 @@
 import { SearchProductsContent } from './SearchProductsContent';
 import { useSearchProductsData } from './searchUtils';
-import { FilterPanel } from 'components/Blocks/Product/Filter/FilterPanel';
+import { FilteredProductsWrapper } from 'components/Blocks/FilteredProductsWrapper/FilteredProductsWrapper';
+import { DeferredFilterPanel } from 'components/Blocks/Product/Filter/DeferredFilterPanel';
 import { FilterSelectedParameters } from 'components/Blocks/Product/Filter/FilterSelectedParameters';
 import { DeferredFilterAndSortingBar } from 'components/Blocks/SortingBar/DeferredFilterAndSortingBar';
 import { useDomainConfig } from 'components/providers/DomainConfigProvider';
 import { TypeProductOrderingModeEnum } from 'graphql/types';
 import useTranslation from 'next-translate/useTranslation';
-import dynamic from 'next/dynamic';
-import { useRef, useState } from 'react';
-import { twJoin } from 'tailwind-merge';
+import { useRef } from 'react';
 import { getInternationalizedStaticUrls } from 'utils/staticUrls/getInternationalizedStaticUrls';
-
-const Overlay = dynamic(() => import('components/Basic/Overlay/Overlay').then((component) => component.Overlay));
 
 export const SearchProducts: FC = () => {
     const { t } = useTranslation();
     const paginationScrollTargetRef = useRef<HTMLDivElement>(null);
     const { url } = useDomainConfig();
     const [searchUrl] = getInternationalizedStaticUrls(['/search'], url);
-    const [isPanelOpen, setIsPanelOpen] = useState(false);
 
     const { searchProductsData, areSearchProductsFetching, isLoadingMoreSearchProducts } = useSearchProductsData();
-
-    const handlePanelOpenerClick = () => {
-        const body = document.getElementsByTagName('body')[0];
-
-        setIsPanelOpen((prev) => {
-            const newValue = !prev;
-            body.style.overflow = newValue ? 'hidden' : 'visible';
-
-            return newValue;
-        });
-    };
 
     if (!searchProductsData) {
         return null;
@@ -41,32 +26,21 @@ export const SearchProducts: FC = () => {
         <>
             <h5 className="mb-2 mt-5 lg:my-9">{t('Found products')}</h5>
 
-            <div className="mb-8 flex scroll-mt-5 flex-col vl:mb-10 vl:flex-row vl:flex-wrap vl:gap-4">
-                <div
-                    className={twJoin(
-                        'fixed bottom-0 left-0 right-10 top-0 max-w-[400px] -translate-x-full overflow-hidden transition max-vl:z-aboveOverlay vl:static vl:w-[227px] vl:translate-x-0 vl:rounded-none vl:transition-none',
-                        isPanelOpen && 'translate-x-0',
-                    )}
-                >
-                    <FilterPanel
-                        defaultOrderingMode={searchProductsData.defaultOrderingMode}
-                        orderingMode={searchProductsData.orderingMode}
-                        originalSlug={null}
-                        panelCloseHandler={handlePanelOpenerClick}
-                        productFilterOptions={searchProductsData.productFilterOptions}
-                        slug={searchUrl}
-                        totalCount={searchProductsData.totalCount}
-                    />
-                </div>
-
-                <Overlay isActive={isPanelOpen} onClick={handlePanelOpenerClick} />
+            <FilteredProductsWrapper paginationScrollTargetRef={paginationScrollTargetRef}>
+                <DeferredFilterPanel
+                    defaultOrderingMode={searchProductsData.defaultOrderingMode}
+                    orderingMode={searchProductsData.orderingMode}
+                    originalSlug={null}
+                    productFilterOptions={searchProductsData.productFilterOptions}
+                    slug={searchUrl}
+                    totalCount={searchProductsData.totalCount}
+                />
 
                 <div className="flex flex-1 flex-col" ref={paginationScrollTargetRef}>
                     <div className="flex flex-col-reverse vl:flex-col">
                         <FilterSelectedParameters filterOptions={searchProductsData.productFilterOptions} />
 
                         <DeferredFilterAndSortingBar
-                            handlePanelOpenerClick={handlePanelOpenerClick}
                             sorting={searchProductsData.orderingMode}
                             totalCount={searchProductsData.totalCount}
                             customSortOptions={[
@@ -84,7 +58,7 @@ export const SearchProducts: FC = () => {
                         searchProductsData={searchProductsData}
                     />
                 </div>
-            </div>
+            </FilteredProductsWrapper>
         </>
     );
 };