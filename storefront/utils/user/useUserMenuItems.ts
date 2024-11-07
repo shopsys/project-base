98aa7bb7065d0e304cfede8084567b8fb45c21f4
Jan Górecki <jan.gorecki@shopsys.com>
2024-11-07 14:55:18 +0100

    add icons to menu on customer pages

--- storefront/utils/user/useUserMenuItems.ts
+++ storefront/utils/user/useUserMenuItems.ts
@@ -1,3 +1,10 @@
+import { CompareIcon } from 'components/Basic/Icon/CompareIcon';
+import { ComplaintsIcon } from 'components/Basic/Icon/ComplaintsIcon';
+import { EditIcon } from 'components/Basic/Icon/EditIcon';
+import { HeartIcon } from 'components/Basic/Icon/HeartIcon';
+import { LockCheckIcon } from 'components/Basic/Icon/LockCheck';
+import { SearchListIcon } from 'components/Basic/Icon/SearchListIcon';
+import { UserIcon } from 'components/Basic/Icon/UserIcon';
 import { useDomainConfig } from 'components/providers/DomainConfigProvider';
 import useTranslation from 'next-translate/useTranslation';
 import { PageType } from 'store/slices/createPageLoadingStateSlice';
@@ -10,6 +17,7 @@ type UserMenuItemType = {
     link: string;
     text: string;
     count?: number;
+    iconComponent?: React.ElementType;
     type?: PageType;
 };
 
@@ -45,33 +53,39 @@ export const useUserMenuItems = (): UserMenuItemType[] => {
             text: t('Orders'),
             link: customerOrdersUrl,
             type: 'orderList',
+            iconComponent: SearchListIcon,
         },
         {
             text: t('Complaints'),
             link: customerComplaintsUrl,
             type: 'complaintList',
+            iconComponent: ComplaintsIcon,
         },
         {
             text: t('Edit profile'),
             link: customerEditProfileUrl,
             type: 'editProfile',
+            iconComponent: EditIcon,
         },
         {
             text: t('Change password'),
             link: customerChangePasswordUrl,
             type: 'changePassword',
+            iconComponent: LockCheckIcon,
         },
         {
             text: t('Wishlist'),
             link: wishlistUrl,
             count: wishlist?.products.length,
             type: 'wishlist',
+            iconComponent: HeartIcon,
         },
         {
             text: t('Comparison'),
             link: productComparisonUrl,
             count: comparison?.products.length,
             type: 'comparison',
+            iconComponent: CompareIcon,
         },
     ];
 
@@ -85,6 +99,7 @@ export const useUserMenuItems = (): UserMenuItemType[] => {
             text: t('Customer users'),
             link: customerUsersUrl,
             type: 'customer-users',
+            iconComponent: UserIcon,
         },
         ...userMenuItems.slice(2),
     ];
