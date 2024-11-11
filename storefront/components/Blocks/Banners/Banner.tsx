1b87fda57bbfe32f586e0ee11b03b79130bb64eb
Jan Molcik <molcik.jan@gmail.com>
2024-11-11 12:41:12 +0000

    add banner description  - the dynamic background color property is build with `getRGBColorString` function as set as element's style as tailwind cannot handle dynamic class names  - the color of the desription text is computed via `getYIQContrastTextColor` to align with the accessibility recommendations (black-ish text for lighter bg and white text for darker bg)

---
+++ storefront/components/Blocks/Banners/Banner.tsx
@@ -0,0 +1,53 @@
+import { BannerImage } from './BannerImage';
+import { CarouselState, getBannerOrderCSSProperty, getRGBColorString, getYIQContrastTextColor } from './bannersUtils';
+import { TypeSliderItemFragment } from 'graphql/requests/sliderItems/fragments/SliderItemFragment.generated';
+import { twJoin } from 'tailwind-merge';
+import { twMergeCustom } from 'utils/twMerge';
+
+type BannerProps = {
+    banner: TypeSliderItemFragment;
+    bannerSliderState: CarouselState;
+    index: number;
+    numItems: number;
+};
+
+const BannerContent: FC<{ banner: TypeSliderItemFragment }> = ({ banner, className }) => (
+    <div
+        className={twMergeCustom(
+            'mt-auto p-6 lg:absolute lg:right-0 lg:h-full lg:w-[455px] lg:px-14 lg:py-24',
+            className,
+        )}
+        style={{
+            backgroundColor: getRGBColorString(banner.rgbBackgroundColor, banner.opacity),
+        }}
+    >
+        <h1 className={twJoin('mb-2.5 vl:mb-5', getYIQContrastTextColor(banner.rgbBackgroundColor))}>{banner.name}</h1>
+        {banner.description && (
+            <p className={getYIQContrastTextColor(banner.rgbBackgroundColor)}>{banner.description}</p>
+        )}
+    </div>
+);
+
+export const Banner: FC<BannerProps> = ({ banner, bannerSliderState, index, numItems }) => {
+    return (
+        <div
+            key={banner.link}
+            className="flex flex-[1_0_100%] basis-full flex-col vl:flex-row"
+            style={{
+                order: getBannerOrderCSSProperty(index, bannerSliderState.sliderPosition, numItems),
+            }}
+        >
+            <BannerImage
+                desktopAlt={banner.webMainImage.name || banner.name}
+                desktopSrc={banner.webMainImage.url}
+                isFirst={index === 0}
+                mobileAlt={banner.mobileMainImage.name || banner.name}
+                mobileSrc={banner.mobileMainImage.url}
+            >
+                <BannerContent banner={banner} className="hidden lg:block" />
+            </BannerImage>
+
+            <BannerContent banner={banner} />
+        </div>
+    );
+};
--- storefront/components/Blocks/Banners/bannersUtils.ts
+++ storefront/components/Blocks/Banners/bannersUtils.ts
@@ -1,4 +1,4 @@
-type CarouselState = {
+export type CarouselState = {
     sliderPosition: number;
     isSliding: boolean;
     slideDirection: 'PREV' | 'NEXT';
@@ -40,4 +40,25 @@ export const bannersReducer = (state: CarouselState, action: CarouselAction): Ca
         default:
             return state;
     }
+};
+
+const getRGBFromHex = (hex: string) => {
+    const color = hex.replace(/#/g, '');
+    return {
+        r: parseInt(color.slice(0, 2), 16),
+        g: parseInt(color.slice(2, 4), 16),
+        b: parseInt(color.slice(4, 6), 16),
+    };
+};
+
+export const getRGBColorString = (hex: string, opacity: number) => {
+    const { r, g, b } = getRGBFromHex(hex);
+    return `rgb(${r} ${g} ${b} / ${opacity})`;
+};
+
+export const getYIQContrastTextColor = (hex: string) => {
+    const { r, g, b } = getRGBFromHex(hex);
+    const yiq = (r * 299 + g * 587 + b * 114) / 1000;
+
+    return yiq >= 128 ? 'text-text' : 'text-textInverted';
 };
