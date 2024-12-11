26af15c1a8d69e1e0bc909ed9e4b35b98a93abed
Rostislav Vitek <rostislav.vitek@shopsys.com>
2024-12-11 13:00:56 +0100

    admin: administrator reset password template now can be send on click to the desired email

--- app/src/Model/Customer/Mail/CustomerMailFacade.php
+++ app/src/Model/Customer/Mail/CustomerMailFacade.php
@@ -14,7 +14,7 @@ use Shopsys\FrameworkBundle\Model\Customer\Mail\CustomerMailFacade as BaseCustom
  * @method __construct(\Shopsys\FrameworkBundle\Model\Mail\Mailer $mailer, \App\Model\Mail\MailTemplateFacade $mailTemplateFacade, \Shopsys\FrameworkBundle\Model\Customer\Mail\RegistrationMail $registrationMail, \App\Component\UploadedFile\UploadedFileFacade $uploadedFileFacade, \App\Model\Customer\Mail\CustomerActivationMail $customerActivationMail)
  * @method sendActivationMail(\App\Model\Customer\User\CustomerUser $customerUser)
  * @method sendRegistrationMailTemplate(\App\Model\Mail\MailTemplate $mailTemplate, \App\Model\Customer\User\CustomerUser $customerUser, string|null $forceSendTo = null)
- * @method sendActivationMailTemplate(\App\Model\Mail\MailTemplate $mailTemplate, \Shopsys\FrameworkBundle\Model\Customer\User\ResetPasswordInterface $customerUser, string|null $forceSendTo = null)
+ * @method sendActivationMailTemplate(\App\Model\Mail\MailTemplate $mailTemplate, \Shopsys\FrameworkBundle\Component\Security\ResetPasswordInterface $customerUser, string|null $forceSendTo = null)
  */
 class CustomerMailFacade extends BaseCustomerMailFacade
 {
