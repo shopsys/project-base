42e6c97ff2ea7dba79c99cd4309db9d7426a2715
Jan Molcik <molcik.jan@gmail.com>
2024-12-17 12:57:48 +0100

    add more identifiers & fix error  - to fix unhandled error, `logException` is used instead of throwing new Error on missing pathname in `RECOMMENDER_PATHNAMES`  - added more pathname options from where a product may be added to cart, resulting in the `RecommendedProductsQuery` being refetched due to `AddToCartPopup` containing `DeferredRecommendedProducts`

--- storefront/utils/recommender/getRecommenderClientIdentifier.ts
+++ storefront/utils/recommender/getRecommenderClientIdentifier.ts
@@ -1,13 +1,22 @@
+import { logException } from 'utils/errors/logException';
+
 const RECOMMENDER_PATHNAMES = {
     '/': 'homepage',
-    '/products/[productSlug]': 'product-detail',
+    '/blogArticles/[blogArticleSlug]': 'blog-article-detail',
     '/cart': 'cart',
+    '/categories/[categorySlug]': 'category-detail',
+    '/flags/[flagSlug]': 'flag-detail',
+    '/product-comparison': 'product-comparison',
+    '/products/[productSlug]': 'product-detail',
+    '/search': 'search',
+    '/wishlist': 'wishlist',
 } as const;
 
 export const getRecommenderClientIdentifier = (pathname: string): string => {
     const splitPathname = pathname.split('?')[0];
     if (!(splitPathname in RECOMMENDER_PATHNAMES)) {
-        throw new Error(`Pathname ${splitPathname} does not have a corresponding recommender client identifier`);
+        logException(`Pathname ${splitPathname} does not have a corresponding identifier in RECOMMENDER_PATHNAMES`);
+        return splitPathname;
     }
 
     return RECOMMENDER_PATHNAMES[splitPathname as RecommenderClientIdentifierKeyType];
