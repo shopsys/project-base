karan-batavia/shopsys

add gtm payment event & fix payment_change event  - payment event is fired for each payment attempt  - `payment_change` event is now also fired from the `order-payment-confirmation` pages   - it is fired from `changePaymentInOrderHandler` after successful payment change

project-base/storefront/components/PaymentsInOrderSelect/PaymentsInOrderSelect.tsx
@@ -56,6 +56,7 @@ export const PaymentsInOrderSelect: FC<PaymentsInOrderSelectProps> = ({
             const changePaymentInOrderData = await changePaymentInOrderHandler(
                 orderUuid,
                 selectedPaymentForChange.uuid,
+                selectedPaymentForChange.type,
                 selectedPaymentSwiftForChange,
                 selectedPaymentForChange.type !== PaymentTypeEnum.GoPay && withRedirectAfterChanging,
             );

project-base/storefront/gtm/factories/getGtmPaymentEvent.ts
@@ -1,0 +1,20 @@
+import { GtmEventType } from 'gtm/enums/GtmEventType';
+import { GtmPaymentEventType } from 'gtm/types/events';
+
+export const getGtmPaymentEvent = (
+    paymentUuid: string,
+    paymentType: string,
+    isPaymentSuccessful: boolean,
+    paymentRetryCount: number,
+    paymentFalseReason?: string,
+): GtmPaymentEventType => ({
+    event: GtmEventType.payment,
+    ecommerce: {
+        id: paymentUuid,
+        isPaymentSuccessful,
+        paymentRetryCount,
+        PaymentFalseReason: paymentFalseReason,
+        paymentType,
+    },
+    _clear: true,
+});

shopsys/project-base

storefront/components/PaymentsInOrderSelect/PaymentsInOrderSelect.tsx
@@ -56,6 +56,7 @@ export const PaymentsInOrderSelect: FC<PaymentsInOrderSelectProps> = ({
             const changePaymentInOrderData = await changePaymentInOrderHandler(
                 orderUuid,
                 selectedPaymentForChange.uuid,
+                selectedPaymentForChange.name,
                 selectedPaymentSwiftForChange,
                 selectedPaymentForChange.type !== PaymentTypeEnum.GoPay && withRedirectAfterChanging,
             );

storefront/gtm/factories/getGtmPaymentEvent.ts
@@ -1,0 +1,20 @@
+import { GtmEventType } from 'gtm/enums/GtmEventType';
+import { GtmPaymentEventType } from 'gtm/types/events';
+
+export const getGtmPaymentEvent = (
+    orderNumber: string,
+    paymentName: string,
+    isPaymentSuccessful: boolean,
+    paymentRetryCount: number,
+    paymentFalseReason?: string,
+): GtmPaymentEventType => ({
+    event: GtmEventType.payment,
+    ecommerce: {
+        id: orderNumber,
+        isPaymentSuccessful,
+        paymentRetryCount,
+        PaymentFalseReason: paymentFalseReason,
+        paymentType: paymentName,
+    },
+    _clear: true,
+});